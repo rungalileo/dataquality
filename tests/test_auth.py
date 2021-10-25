0ed1f9929957fcdcc1a09f6e4f3bcf685ebd999f
Ben Epstein <ben.epstein@wustl.edu>
2021-10-25 13:06:16 -0400

    mock user

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,20 +5,21 @@ import pytest
 
 import dataquality
 from dataquality.core.auth import GALILEO_AUTH_METHOD
-from tests.utils.mock_request import mocked_failed_requests_post, mocked_requests_post
+from tests.utils.mock_request import mocked_failed_requests, mocked_requests
 
 config = dataquality.config
 
 
-@mock.patch("requests.post", side_effect=mocked_requests_post)
-def test_good_login(mock_post) -> None:
+@mock.patch("requests.post", side_effect=mocked_requests)
+@mock.patch("requests.get", side_effect=mocked_requests)
+def test_good_login(*args) -> None:
     os.environ[GALILEO_AUTH_METHOD] = "email"
     os.environ["GALILEO_USERNAME"] = "user"
     os.environ["GALILEO_PASSWORD"] = "password"
     dataquality.login()
 
 
-@mock.patch("requests.post", side_effect=mocked_failed_requests_post)
+@mock.patch("requests.post", side_effect=mocked_failed_requests)
 def test_bad_login(mock_post) -> None:
     config.token = None
     os.environ[GALILEO_AUTH_METHOD] = "email"
--- tests/utils/mock_request.py
+++ tests/utils/mock_request.py
@@ -5,7 +5,7 @@ import dataquality
 config = dataquality.config
 
 
-def mocked_requests_post(*args: Any, **kwargs: Dict[str, Any]):
+def mocked_requests(*args: Any, **kwargs: Dict[str, Any]):
     class MockResponse:
         def __init__(self, json_data, status_code):
             self.json_data = json_data
@@ -17,10 +17,13 @@ def mocked_requests_post(*args: Any, **kwargs: Dict[str, Any]):
     if args[0].endswith("login"):
         return MockResponse({"access_token": "mock_token"}, 200)
 
-    return MockResponse(None, 404)
+    if args[0].endswith("current_user"):
+        return MockResponse({"user": "user"}, 200)
+
+    return MockResponse(None, 200)
 
 
-def mocked_failed_requests_post(*args: Any, **kwargs: Dict[str, Any]):
+def mocked_failed_requests(*args: Any, **kwargs: Dict[str, Any]):
     class MockResponse:
         def __init__(self, json_data, status_code):
             self.json_data = json_data
@@ -30,6 +33,6 @@ def mocked_failed_requests_post(*args: Any, **kwargs: Dict[str, Any]):
             return self.json_data
 
     if args[0].endswith("login"):
-        return MockResponse({"err": "err"}, 404)
+        return MockResponse("Invalid credentials", 404)
 
     return MockResponse(None, 404)
