9b33f06308f1de3218d43b9bc851db61d228f40c
Ben Epstein <ben.epstein@wustl.edu>
2021-11-10 10:13:27 -0500

    set resource limit on import

--- dataquality/__init__.py
+++ dataquality/__init__.py
@@ -2,13 +2,14 @@
 
 __version__ = "0.0.2"
 
+import resource
+
 from dataquality.core.auth import login
 from dataquality.core.config import config
 from dataquality.core.finish import finish
 from dataquality.core.init import init
 from dataquality.core.log import (
     log_batch_input_data,
-    log_input_data,
     log_model_outputs,
     set_labels_for_run,
 )
@@ -19,10 +20,11 @@ __all__ = [
     "login",
     "init",
     "log_batch_input_data",
-    "log_input_data",
     "log_model_outputs",
     "config",
     "finish",
     "set_labels_for_run",
     "GalileoException",
 ]
+
+resource.setrlimit(resource.RLIMIT_NOFILE, (65535, 65535))
