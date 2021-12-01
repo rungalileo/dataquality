91d0e1e2b8c0f4499ebafa4f6f6311ec28f1a8b0
Nikita <nikita@rungalileo.io>
2021-12-01 11:22:00 -0600

    Formatting

--- tests/integration/mock_training_run.py
+++ tests/integration/mock_training_run.py
@@ -8,27 +8,29 @@ https://s3.console.aws.amazon.com/s3/buckets/galileo-ml-train?region=us-west-
 
 # Set environment flags for dataquality import
 import os
-os.environ['GALILEO_API_URL'] = "https://api.dev.rungalileo.io"
-os.environ['GALILEO_MINIO_URL'] = "data.dev.rungalileo.io"
-os.environ['GALILEO_MINIO_ACCESS_KEY'] = "minioadmin"
-os.environ['GALILEO_MINIO_SECRET_KEY'] = "minioadmin"
-os.environ['GALILEO_MINIO_REGION'] = "us-east-1"
+
+os.environ["GALILEO_API_URL"] = "https://api.dev.rungalileo.io"
+os.environ["GALILEO_MINIO_URL"] = "data.dev.rungalileo.io"
+os.environ["GALILEO_MINIO_ACCESS_KEY"] = "minioadmin"
+os.environ["GALILEO_MINIO_SECRET_KEY"] = "minioadmin"
+os.environ["GALILEO_MINIO_REGION"] = "us-east-1"
 os.environ["GALILEO_AUTH_METHOD"] = "email"
 os.environ["GALILEO_USERNAME"] = "adminy_guy@rungalileo.io"
 os.environ["GALILEO_PASSWORD"] = "Admin123@"
-import dataquality
-from pathlib import Path
-import pandas as pd
 import time
-from dataquality.core.integrations.config import GalileoDataConfig
-from dataquality.core.integrations.config import GalileoModelConfig
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 from alive_progress import alive_it
+
+import dataquality
+from dataquality.core.integrations.config import GalileoDataConfig, GalileoModelConfig
 
 DATASET = "trec6"
 TRAIN_DATASET_NAME = f"{DATASET}_train.csv"
 TEST_DATASET_NAME = f"{DATASET}_test.csv"
-DATASET_FOLDER_PATH = Path("galileo-ml-train")/"datasets"/"original"/DATASET
+DATASET_FOLDER_PATH = Path("galileo-ml-train") / "datasets" / "original" / DATASET
 
 NUM_EPOCHS = 5
 BATCH_SIZE = 32
@@ -36,9 +38,7 @@ EMB_DIM = 768
 
 
 def download_dataset_from_aws(dataset_folder_path: str) -> None:
-    cmd = (
-        f"aws s3 sync s3://{dataset_folder_path} ."
-    )
+    cmd = f"aws s3 sync s3://{dataset_folder_path} ."
     os.system(cmd)
 
 
@@ -53,10 +53,10 @@ def generate_random_embeddings(batch_size: int, emb_dims: int) -> np.ndarray:
 
 def generate_random_probabilities(batch_size: int, num_classes: int) -> np.ndarray:
     probs = np.random.rand(batch_size, num_classes)
-    return probs / probs.sum(axis=-1).reshape(-1, 1) # Normalize to sum to 1
+    return probs / probs.sum(axis=-1).reshape(-1, 1)  # Normalize to sum to 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     download_dataset_from_aws(DATASET_FOLDER_PATH)
 
     train_dataset = load_dataset_split(DATASET, "train")
@@ -66,18 +66,22 @@ if __name__ == '__main__':
     dataquality.init(project_name="test_large_dataset", run_name=DATASET)
 
     t_start = time.time()
-    dataquality.log_batch_input_data(GalileoDataConfig(
-        text=train_dataset["text"],
-        labels=train_dataset["label"],
-        ids=train_dataset["id"],
-        split="train"
-    ))
-    dataquality.log_batch_input_data(GalileoDataConfig(
-        text=test_dataset["text"],
-        labels=test_dataset["label"],
-        ids=test_dataset["id"],
-        split="test"
-    ))
+    dataquality.log_batch_input_data(
+        GalileoDataConfig(
+            text=train_dataset["text"],
+            labels=train_dataset["label"],
+            ids=train_dataset["id"],
+            split="train",
+        )
+    )
+    dataquality.log_batch_input_data(
+        GalileoDataConfig(
+            text=test_dataset["text"],
+            labels=test_dataset["label"],
+            ids=test_dataset["id"],
+            split="test",
+        )
+    )
     print(f"Took {time.time() - t_start} seconds")
 
     t_start = time.time()
@@ -88,23 +92,35 @@ if __name__ == '__main__':
         # Train
         print("Training")
         for i in alive_it(range(0, len(train_dataset), BATCH_SIZE)):
-            batch = train_dataset[i:i+BATCH_SIZE]
+            batch = train_dataset[i : i + BATCH_SIZE]
 
             embedding = generate_random_embeddings(len(batch), EMB_DIM)
             probs = generate_random_probabilities(len(batch), num_classes)
 
-            dataquality.log_model_outputs(GalileoModelConfig(
-                emb=embedding, probs=probs, split="train", epoch=epoch_idx, ids=batch["id"]
-            ))
+            dataquality.log_model_outputs(
+                GalileoModelConfig(
+                    emb=embedding,
+                    probs=probs,
+                    split="train",
+                    epoch=epoch_idx,
+                    ids=batch["id"],
+                )
+            )
         # Test
         print("Testing")
         for i in alive_it(range(0, len(test_dataset), BATCH_SIZE)):
-            batch = train_dataset[i:i+BATCH_SIZE]
+            batch = train_dataset[i : i + BATCH_SIZE]
 
             embedding = generate_random_embeddings(len(batch), EMB_DIM)
             probs = generate_random_probabilities(len(batch), num_classes)
 
-            dataquality.log_model_outputs(GalileoModelConfig(
-                emb=embedding, probs=probs, split="test", epoch=epoch_idx, ids=batch["id"]
-            ))
+            dataquality.log_model_outputs(
+                GalileoModelConfig(
+                    emb=embedding,
+                    probs=probs,
+                    split="test",
+                    epoch=epoch_idx,
+                    ids=batch["id"],
+                )
+            )
     print(f"Took {time.time() - t_start} seconds")
