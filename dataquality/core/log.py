3538a34c055ae5693a1455a1d0f114b2ef95d72c
Ben Epstein <ben@rungalileo.io>
2021-12-01 18:53:08 -0500

    use vaex for everything

--- dataquality/core/log.py
+++ dataquality/core/log.py
@@ -1,12 +1,14 @@
 import os
 import threading
+from glob import glob
 from typing import Dict, List
 from uuid import uuid4
 
 import numpy as np
 import pandas as pd
-from pyarrow.lib import ArrowException, ArrowIOError
+import vaex
 from pydantic.error_wrappers import ValidationError
+from vaex.dataframe import DataFrame
 
 from dataquality import config
 from dataquality.core.integrations.config import GalileoDataConfig, GalileoModelConfig
@@ -18,7 +20,7 @@ from dataquality.schemas.split import Split
 from dataquality.utils.thread_pool import ThreadPoolManager
 
 DATA_FOLDERS = ["emb", "prob", "data"]
-INPUT_DATA_NAME = "input_data.pkl"
+INPUT_DATA_NAME = "input_data.arrow"
 
 
 class Logger:
@@ -67,19 +69,22 @@ def log_batch_input_data(data: GalileoDataConfig) -> None:
     )
     if not os.path.exists(write_input_dir):
         os.makedirs(write_input_dir)
-    df = pd.DataFrame(
-        dict(
-            id=data.ids,
-            text=data.text,
-            split=data.split,
-            data_schema_version=1,
-            gold=data.labels if data.split != Split.validation else None,
+    df = vaex.from_pandas(
+        pd.DataFrame(
+            dict(
+                id=data.ids,
+                text=data.text,
+                split=data.split,
+                data_schema_version=1,
+                gold=data.labels if data.split != Split.validation else None,
+            )
         )
     )
     file_path = f"{write_input_dir}/{INPUT_DATA_NAME}"
     if os.path.isfile(file_path):
-        df = pd.concat([df, pd.read_pickle(file_path)])
-    df.to_pickle(file_path)
+        df = vaex.concat([df, vaex.open(file_path)])
+    df.export_arrow(file_path)
+    df.close()
 
 
 def validate_model_output(data: Dict) -> JsonlOutputLogItem:
@@ -106,7 +111,7 @@ def set_labels_for_run(labels: List[str]) -> None:
     config.update_file_config()
 
 
-def _log_model_outputs(outputs: GalileoModelConfig) -> None:
+def _log_model_outputs_target(outputs: GalileoModelConfig) -> None:
     """
     Threaded child function for logging model outputs. Used as target for
     log_model_outputs
@@ -130,6 +135,13 @@ def _log_model_outputs(outputs: GalileoModelConfig) -> None:
         record = validate_model_output(record).dict()
         data.append(record)
     write_model_output(model_output=pd.DataFrame(data))
+
+
+def _log_model_outputs(outputs: GalileoModelConfig) -> None:
+    try:
+        _log_model_outputs_target(outputs)
+    except Exception as e:
+        print(f"An error occurred while logging: {e}")
 
 
 def log_model_outputs(outputs: GalileoModelConfig) -> None:
@@ -156,43 +168,54 @@ def write_model_output(model_output: pd.DataFrame) -> None:
         f"{JsonlLogger.LOG_FILE_DIR}/{config.current_project_id}"
         f"/{config.current_run_id}"
     )
-    in_frame_dtypes = {"gold": "object"}
-    out_frame_dtypes = {"pred": "int64"}
-    in_frame = pd.read_pickle(f"{location}/{INPUT_DATA_NAME}").astype(in_frame_dtypes)
-    out_frame = model_output.astype(dtype=out_frame_dtypes)
 
-    in_out = out_frame.merge(
-        in_frame, on=["split", "id", "data_schema_version"], how="left"
+    config.observed_num_labels = len(model_output["prob"].values[0])
+    n_emb = len(model_output["emb"][0])
+    # We want embeddings to be a wide dataframe 1 column per emb
+    emb_cols = [f"emb_{i}" for i in range(n_emb)]
+    model_output[emb_cols] = pd.DataFrame(
+        model_output["emb"].tolist(), columns=emb_cols
     )
 
-    config.observed_num_labels = len(out_frame["prob"].values[0])
+    out_frame_dtypes = {"pred": "int64"}
+    in_frame = vaex.open(f"{location}/{INPUT_DATA_NAME}").copy()
+
+    out_frame = vaex.from_pandas(model_output.astype(dtype=out_frame_dtypes))
+
+    in_frame["split_id"] = in_frame["split"] + in_frame["id"].astype("string")
+    out_frame["split_id"] = out_frame["split"] + out_frame["id"].astype("string")
+
+    in_out = out_frame.join(
+        in_frame, on="split_id", how="left", lsuffix="_L", rsuffix="_R"
+    ).copy()
+    drop_cols = [c for c in in_out.get_column_names() if not c.endswith("_L")]
+    in_out = in_out[drop_cols]
+    for c in in_out.get_column_names():
+        if c.endswith("_R"):
+            in_out.rename(c, c.rstrip("_R"))
 
     # Separate out embeddings and probabilities into their own arrow files
-    emb = in_out[["id", "emb"]]
-    n_emb = len(emb["emb"][0])
-    # We want embeddings to be a wide dataframe 1 column per emb
-    cols = [f"emb_{i}" for i in range(n_emb)]
-    emb_wide = pd.DataFrame(emb["emb"].tolist(), columns=cols)
-    emb_wide.insert(loc=0, column="id", value=emb["id"])
-
     prob = in_out[["id", "prob", "gold"]]
-    other_cols = [i for i in in_out.columns if i not in ["emb", "prob"]]
+    emb_wide = in_out[["id"] + emb_cols]
+    ignore_cols = emb_cols + ["emb", "prob", "split_id"]
+    other_cols = [i for i in in_out.columns if i not in ignore_cols]
     in_out = in_out[other_cols]
 
     # Each input file will have all records from the same split and epoch, so we can
     # store them in minio partitioned by split/epoch
-    epoch, split = in_out[["epoch", "split"]].loc[0]
+    epoch, split = in_out[["epoch", "split"]][0]
 
     # Random name to avoid collisions
     object_name = f"{str(uuid4()).replace('-', '')[:12]}.arrow"
     for file, data_name in zip([emb_wide, prob, in_out], DATA_FOLDERS):
         path = f"{location}/{split}/{epoch}/{data_name}"
         _save_arrow_file(path, object_name, file)
+        file.close()
 
 
-def _save_arrow_file(location: str, file_name: str, file: pd.DataFrame) -> None:
+def _save_arrow_file(location: str, file_name: str, file: DataFrame) -> None:
     """
-    Helper function to save a pandas dataframe as an arrow file. We use the
+    Helper function to save a vaex dataframe as an arrow file. We use the
     to_feather function as the wrapper to arrow.
 
     We try to save the file with zstd compression first, falling back to default
@@ -202,10 +225,13 @@ def _save_arrow_file(location: str, file_name: str, file: pd.DataFrame) -> None:
     with lock:
         if not os.path.isdir(location):
             os.makedirs(location)
-
-    file_path = f"{location}/{file_name}"
-    try:
-        file.to_feather(file_path, compression="zstd")
-    # In case zstd compression is not available
-    except (ArrowException, ArrowIOError):
-        file.to_feather(file_path)
+        file_path = f"{location}/{file_name}"
+        file.export_arrow(file_path)
+        new_name = f"{str(uuid4()).replace('-', '')[:12]}.arrow"
+        arrow_files = glob(f"{location}/*.arrow")
+        if len(arrow_files) > 25:
+            df = vaex.open_many(arrow_files)
+            df.export_arrow(f"{location}/{new_name}")
+            df.close()
+            for f in arrow_files:
+                os.remove(f)
