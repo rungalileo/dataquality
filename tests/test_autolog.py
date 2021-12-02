94ebb2757bfc1aca8d7c15c46fbab92ab1c5500a
Ben Epstein <ben@rungalileo.io>
2021-12-02 09:16:53 -0500

    test in memory cache for vaex

--- tests/test_autolog.py
+++ tests/test_autolog.py
@@ -1,5 +1,6 @@
 import pytorch_lightning as pl
 import torch
+import vaex
 
 from dataquality.core.finish import _cleanup, _upload
 from dataquality.core.integrations.lightning import DataQualityCallback
@@ -19,76 +20,78 @@ def test_lightning_autolog(cleanup_after_use) -> None:
     """
     Tests the lightning autolog config and that data is properly stored / logged
     """
-    ThreadPoolManager.MAX_THREADS = 1  # For github actions
+    with vaex.cache.memory_infinite():
+        train_dataloader = torch.utils.data.DataLoader(
+            NewsgroupDataset("training"), batch_size=NUM_RECORDS, shuffle=True
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            NewsgroupDataset("test"), batch_size=NUM_RECORDS, shuffle=True
+        )
+        trainer = pl.Trainer(
+            max_epochs=1, num_sanity_val_steps=0, callbacks=[(DataQualityCallback())]
+        )
 
-    train_dataloader = torch.utils.data.DataLoader(
-        NewsgroupDataset("training"), batch_size=NUM_RECORDS, shuffle=True
-    )
-    test_dataloader = torch.utils.data.DataLoader(
-        NewsgroupDataset("test"), batch_size=NUM_RECORDS, shuffle=True
-    )
-    trainer = pl.Trainer(
-        max_epochs=1, num_sanity_val_steps=0, callbacks=[(DataQualityCallback())]
-    )
-
-    trainer.fit(model, train_dataloader)
-    trainer.test(model, test_dataloader)
-    ThreadPoolManager.wait_for_threads()
-    # Mock call to finish
-    _upload()
-    validate_uploaded_data(expected_num_records=NUM_RECORDS)
-    _cleanup()
-    validate_cleanup_data()
+        trainer.fit(model, train_dataloader)
+        trainer.test(model, test_dataloader)
+        ThreadPoolManager.wait_for_threads()
+        # Mock call to finish
+        _upload()
+        validate_uploaded_data(expected_num_records=NUM_RECORDS)
+        _cleanup()
+        validate_cleanup_data()
 
 
 def test_torch_autolog(cleanup_after_use) -> None:
     """Tests our watch(model) functionality for pytorch"""
-    train_dataloader = torch.utils.data.DataLoader(
-        NewsgroupDataset("training"), batch_size=NUM_RECORDS, shuffle=True
-    )
-    test_dataloader = torch.utils.data.DataLoader(
-        NewsgroupDataset("test"), batch_size=NUM_RECORDS, shuffle=True
-    )
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    torch_model.to(device)
+    with vaex.cache.memory_infinite():
+        train_dataloader = torch.utils.data.DataLoader(
+            NewsgroupDataset("training"), batch_size=NUM_RECORDS, shuffle=True
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            NewsgroupDataset("test"), batch_size=NUM_RECORDS, shuffle=True
+        )
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+        torch_model.to(device)
 
-    log_input_data(train_dataloader, "training")
-    log_input_data(test_dataloader, "test")
+        log_input_data(train_dataloader, "training")
+        log_input_data(test_dataloader, "test")
 
-    optimizer = torch.optim.AdamW(
-        filter(lambda p: p.requires_grad, torch_model.parameters()), lr=1e-5
-    )
+        optimizer = torch.optim.AdamW(
+            filter(lambda p: p.requires_grad, torch_model.parameters()), lr=1e-5
+        )
 
-    watch(torch_model)
+        watch(torch_model)
 
-    for epoch in range(2):
-        torch_model.train()
-        for data in train_dataloader:
-            x_idxs, x, attention_mask, y = data
-            x = x.to(device)
-            attention_mask = attention_mask.to(device)
-            y = torch.tensor(y, device=device)
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            # forward + backward + optimize
-            _ = torch_model(
-                x, attention_mask, x_idxs=x_idxs, epoch=epoch, split="training"
-            )
-
-        with torch.no_grad():
-            for data in test_dataloader:
+        for epoch in range(2):
+            torch_model.train()
+            for data in train_dataloader:
                 x_idxs, x, attention_mask, y = data
-
                 x = x.to(device)
                 attention_mask = attention_mask.to(device)
+                y = torch.tensor(y, device=device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward + backward + optimize
                 _ = torch_model(
-                    x, attention_mask, epoch=epoch, split="test", x_idxs=x_idxs
+                    x, attention_mask, x_idxs=x_idxs, epoch=epoch, split="training"
                 )
 
-    ThreadPoolManager.wait_for_threads()
-    # Mock call to finish
-    _upload()
-    validate_uploaded_data(expected_num_records=NUM_RECORDS)
-    _cleanup()
-    validate_cleanup_data()
+            with torch.no_grad():
+                for data in test_dataloader:
+                    x_idxs, x, attention_mask, y = data
+
+                    x = x.to(device)
+                    attention_mask = attention_mask.to(device)
+                    _ = torch_model(
+                        x, attention_mask, epoch=epoch, split="test", x_idxs=x_idxs
+                    )
+
+        ThreadPoolManager.wait_for_threads()
+        # Mock call to finish
+        _upload()
+        validate_uploaded_data(expected_num_records=NUM_RECORDS)
+        _cleanup()
+        validate_cleanup_data()
