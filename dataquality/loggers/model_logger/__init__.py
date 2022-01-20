8f1af563518182d6e0e92a7cc3766ba5b462853c
Ben Epstein <ben@rungalileo.io>
2022-01-19 20:21:00 -0500

    add to init

--- dataquality/loggers/model_logger/__init__.py
+++ dataquality/loggers/model_logger/__init__.py
@@ -1,4 +1,13 @@
-from dataquality.loggers.model_logger import text_classification, text_multi_label
+from dataquality.loggers.model_logger import (
+    text_classification,
+    text_multi_label,
+    text_ner,
+)
 from dataquality.loggers.model_logger.base_model_logger import BaseGalileoModelLogger
 
-__all__ = ["text_classification", "text_multi_label", "BaseGalileoModelLogger"]
+__all__ = [
+    "text_classification",
+    "text_multi_label",
+    "text_ner",
+    "BaseGalileoModelLogger",
+]
