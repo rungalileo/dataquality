41c22d9507d7f1cf12679952cba523ea8ef1b797
Ben Epstein <ben@rungalileo.io>
2022-02-09 12:22:41 -0500

    cleaner token set

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,18 +12,21 @@ config = dataquality.config
 
 @mock.patch("requests.post", side_effect=mocked_login_requests)
 @mock.patch("requests.get", side_effect=mocked_login_requests)
-def test_good_login(manage_token, *args) -> None:
+def test_good_login(*args) -> None:
     os.environ[GALILEO_AUTH_METHOD] = "email"
     os.environ["GALILEO_USERNAME"] = "user"
     os.environ["GALILEO_PASSWORD"] = "password"
+    config.token = "mytoken"
     dataquality.login()
 
 
 @mock.patch("requests.post", side_effect=mocked_failed_login_requests)
-def test_bad_login(mock_post, manage_token) -> None:
+def test_bad_login(mock_post) -> None:
+    tok = config.token
     config.token = None
     os.environ[GALILEO_AUTH_METHOD] = "email"
     os.environ["GALILEO_USERNAME"] = "user"
     os.environ["GALILEO_PASSWORD"] = "password"
     with pytest.raises(Exception):
         dataquality.login()
+    config.token = tok
