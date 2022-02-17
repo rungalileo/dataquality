eb37be9faa145af7e3fbc974878e1749e1bb9374
Anthony Corletti <anthony@rungalileo.io>
2022-02-17 12:08:58 -0500

    housekeeping: remove args and kwargs in test (#138)

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 import os
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -12,7 +13,10 @@ config = dataquality.config
 
 @mock.patch("requests.post", side_effect=mocked_login_requests)
 @mock.patch("requests.get", side_effect=mocked_login_requests)
-def test_good_login(*args) -> None:
+def test_good_login(
+    mock_get_current_user: MagicMock,
+    mock_login: MagicMock,
+) -> None:
     os.environ[GALILEO_AUTH_METHOD] = "email"
     os.environ["GALILEO_USERNAME"] = "user"
     os.environ["GALILEO_PASSWORD"] = "password"
@@ -21,7 +25,7 @@ def test_good_login(*args) -> None:
 
 
 @mock.patch("requests.post", side_effect=mocked_failed_login_requests)
-def test_bad_login(mock_post) -> None:
+def test_bad_login(mock_post: MagicMock) -> None:
     tok = config.token
     config.token = None
     os.environ[GALILEO_AUTH_METHOD] = "email"
