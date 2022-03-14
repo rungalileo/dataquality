ebe300b6c1963958fbb428e398800612d20911b8
Elliott <69087256+elboy3@users.noreply.github.com>
2022-03-14 10:21:05 -0400

    Feature/issue168/replace proc routes (#169)

--- dataquality/schemas/route.py
+++ dataquality/schemas/route.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from typing import Optional, Union
+
+from pydantic import UUID4
+
+from dataquality.schemas.split import Split
 
 
 class Route(str, Enum):
@@ -6,7 +11,6 @@ class Route(str, Enum):
     List of available API routes
     """
 
-    proc_pool = "proc/pool"
     projects = "projects"
     runs = "runs"
     users = "users"
@@ -14,5 +18,26 @@ class Route(str, Enum):
     login = "login"
     current_user = "current_user"
     healthcheck = "healthcheck"
-    proc = "proc"
     slices = "slices"
+    split_path = "split"
+    jobs = "jobs"
+
+    # content_path = partial(content_path)
+    # def __call__(self, *args: Any) -> None:
+    #     self.value(*args)
+
+    @staticmethod
+    def content_path(
+        project_id: Optional[Union[str, UUID4]] = None,
+        run_id: Optional[Union[str, UUID4]] = None,
+        split: Optional[Union[str, Split]] = None,
+    ) -> str:
+        path = ""
+        if project_id:
+            path += f"/{Route.projects}/{project_id}"
+        if run_id:
+            path += f"/{Route.runs}/{run_id}"
+        if split:
+            path += f"/{Route.split_path}/{split}"
+
+        return path.strip("/")
