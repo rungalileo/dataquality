71f53ae43deb91458dc4a58a6ccd7f0da9971be5
nikita-galileo <83978491+nikita-galileo@users.noreply.github.com>
2022-03-16 15:11:44 -0700

    Small change to lint.sh, have black --check come before flake8
    Since the suggestions black has often will fix flake8, I would propose we change the order? In my linting anyways I notice I go mypy, black, flake8, isort

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
 mypy dataquality
-flake8 dataquality tests docs_src
 black dataquality tests docs_src --check
+flake8 dataquality tests docs_src
 isort dataquality tests docs_src --check-only
