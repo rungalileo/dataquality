f6f016b3bffc103b95d33219e6024ece5561efdc
Elliott <echartock3@gmail.com>
2022-03-21 16:20:36 -0400

    Fix tests

--- tests/inference/test_text_classification.py
+++ tests/inference/test_text_classification.py
@@ -1,19 +1,8 @@
-from typing import Callable, Type
+from typing import Callable
 
 import pytest
 
 import dataquality
-from dataquality.loggers.base_logger import T
-from dataquality.loggers.data_logger.text_classification import (
-    TextClassificationDataLogger,
-)
-from dataquality.loggers.model_logger.text_classification import (
-    TextClassificationModelLogger,
-)
-
-
-def test_inference_e2e():
-    pass
 
 
 def test_set_split_inference(cleanup_after_use: Callable) -> None:
@@ -26,11 +15,6 @@ def test_set_split_inference(cleanup_after_use: Callable) -> None:
     )
 
 
-@pytest.mark.parametrize(
-    "Logger", [TextClassificationDataLogger, TextClassificationModelLogger]
-)
-def test_log_input_data_sets_logged_config(
-    set_test_config: Callable, Logger: Type[T]
-) -> None:
-    logger = Logger(split="inference")
-    assert logger.logger_config.inference_logged
+def test_set_split_inference_missing_inference_name() -> None:
+    with pytest.raises(AssertionError):
+        dataquality.set_split("inference", "all-customers")
