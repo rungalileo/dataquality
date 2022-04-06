43cc4a3ff03449e5259f14e7ccc3cc1a96483607
Nikita <nikita@rungalileo.io>
2022-04-05 23:43:15 -0700

    fix tests with new zeroing out strategy

--- tests/test_spacy_integration.py
+++ tests/test_spacy_integration.py
@@ -9,7 +9,7 @@ def test_convert_spacy_ner_logits_to_valid_logits():
 
     assert np.allclose(
         convert_spacy_ner_logits_to_valid_logits(arr_1, pred_1),
-        [-np.inf, -np.inf, 0.7, 0.6, 0.5, 0.4],
+        [-0.6, -0.6, 0.7, 0.6, 0.5, 0.4],
     )
 
     arr_2 = np.array([3.4, -0.9, 0.4, 2.8])
@@ -24,7 +24,7 @@ def test_convert_spacy_ner_logits_to_valid_logits():
 
     assert np.allclose(
         convert_spacy_ner_logits_to_valid_logits(arr_3, pred_3),
-        [-np.inf, -0.9, -np.inf, -np.inf],
+        [-1.9, -0.9, -1.9, -1.9],
     )
 
     print("pausing")
