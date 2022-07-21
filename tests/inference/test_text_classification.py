3b66ce58833b2a7694b327e06a2dbd15ff0f5384
Ben Epstein <ben@rungalileo.io>
2022-07-21 10:29:44 -0400

    support for inference in log_dataset

--- tests/inference/test_text_classification.py
+++ tests/inference/test_text_classification.py
@@ -1,13 +1,18 @@
 from pathlib import Path
-from typing import List, Type
+from typing import Callable, List, Type
 from unittest import mock
 
+import datasets
+import pandas as pd
 import pytest
+import tensorflow as tf
+import vaex
 from pydantic import ValidationError
 
 import dataquality
 from dataquality.exceptions import GalileoException
 from dataquality.loggers.base_logger import BaseGalileoLogger, T
+from dataquality.loggers.data_logger.base_data_logger import DataSet
 from dataquality.loggers.data_logger.text_classification import (
     TextClassificationDataLogger,
 )
@@ -156,3 +161,58 @@ class TestTextClassificationDataLoggerInference:
             "Inference name must be set when logging an inference split. Use "
             "set_split('inference', inference_name) to set inference name"
         )
+
+    @pytest.mark.parametrize(
+        "dataset",
+        [
+            pd.DataFrame(
+                {
+                    "my_text": ["sample1", "sample2", "sample3"],
+                    "my_id": [1, 2, 3],
+                }
+            ),
+            vaex.from_dict(
+                {
+                    "my_text": ["sample1", "sample2", "sample3"],
+                    "my_id": [1, 2, 3],
+                }
+            ),
+            [
+                {"my_text": "sample1", "my_id": 1},
+                {"my_text": "sample2", "my_id": 2},
+                {"my_text": "sample3", "my_id": 3},
+            ],
+            tf.data.Dataset.from_tensor_slices(
+                {
+                    "my_text": ["sample1", "sample2", "sample3"],
+                    "my_id": [1, 2, 3],
+                }
+            ),
+            datasets.Dataset.from_dict(
+                dict(
+                    my_text=["sample1", "sample2", "sample3"],
+                    my_id=[1, 2, 3],
+                )
+            ),
+        ],
+    )
+    def test_log_dataset(
+        self, dataset: DataSet, set_test_config: Callable, cleanup_after_use: Callable
+    ) -> None:
+        set_test_config(split="inference")
+        logger = TextClassificationDataLogger()
+
+        with mock.patch("dataquality.core.log.get_data_logger") as mock_method:
+            mock_method.return_value = logger
+            dataquality.log_dataset(
+                dataset,
+                text="my_text",
+                id="my_id",
+                split="inference",
+                inference_name="foo",
+            )
+
+            assert logger.texts == ["sample1", "sample2", "sample3"]
+            assert logger.ids == [1, 2, 3]
+            assert logger.split == Split.inference
+            assert logger.inference_name == "foo"
