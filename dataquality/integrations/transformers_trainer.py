3320ae989c87a86ab8944d3c3bfeeacb5724b626
Franz Gusto <fkrekeler@gmail.com>
2022-10-09 11:54:26 -0400

    formatting fix

--- dataquality/integrations/transformers_trainer.py
+++ dataquality/integrations/transformers_trainer.py
@@ -84,9 +84,12 @@ class DQCallback(TrainerCallback):
 
         if isinstance(train_dataloader_ds, Dataset):
             assert "id" in train_dataloader_ds.column_names, GalileoException(
-                "Did you map IDs to your dataset before watching the model? You can run:\n"             
-                """`ds= dataset.map(lambda x, idx: {"id": idx}, with_indices=True)`\n"""
-                " id (index) column is needed in the dataset for logging"
+                """\
+                Did you map IDs to your dataset before watching the model?
+                You can run:
+                `ds= dataset.map(lambda x, idx: {"id": idx}, with_indices=True)`
+                id (index) column is needed in the dataset for logging
+         """
             )
         else:
             raise GalileoException(f"Unknown dataset type {type(train_dataloader_ds)}")
