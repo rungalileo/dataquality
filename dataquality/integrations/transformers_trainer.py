ad47d1932e30ee529748c1615de9b74a75d291e5
Franz Gusto <fkrekeler@gmail.com>
2022-10-11 13:19:12 -0500

    cleanup for adding tests

--- dataquality/integrations/transformers_trainer.py
+++ dataquality/integrations/transformers_trainer.py
@@ -1,11 +1,10 @@
 # Imports for the hook manager
-from queue import Queue
-from typing import Callable, Dict, Iterable, List, Optional, Union
+
+from typing import Dict, Iterable, Optional, Union
 
 from datasets import Dataset
 from torch import Tensor
 from torch.nn import Module
-from torch.utils.hooks import RemovableHandle
 from transformers import Trainer
 from transformers.modeling_outputs import BaseModelOutput, TokenClassifierOutput
 from transformers.trainer_callback import TrainerCallback, TrainerControl, TrainerState
@@ -16,6 +15,7 @@ from dataquality.exceptions import GalileoException
 from dataquality.schemas.split import Split
 from dataquality.schemas.task_type import TaskType
 from dataquality.utils.helpers import check_noop
+from dataquality.utils.torch import HookManager
 from dataquality.utils.transformers import (
     add_id_to_signature_columns,
     remove_id_collate_fn_wrapper,
@@ -225,79 +225,6 @@ class DQCallback(TrainerCallback):
         elif hasattr(model_output, "logits"):
             logits = model_output.logits
         self.helper_data["logits"] = logits.detach()
-
-
-class HookManager:
-    """
-    Manages hooks for models. Has the ability to find the layer automatically.
-    Otherwise the layer or the layer name needs to be provided.
-    """
-
-    # Stores all hooks to remove them from the model later.
-    hooks: List[RemovableHandle] = []
-
-    def get_embedding_layer_auto(self, model: Module) -> Module:
-        """
-        Use a scoring algorithm to find the embedding layer automatically
-        given a model. The higher the score the more likely it is the embedding layer.
-        """
-        name, layer = next(model.named_children())
-        print(f"Selected layer for the last hidden state embedding {name}")
-        return layer
-
-    def get_embedding_layer_by_name(self, model: Module, name: str) -> Module:
-        """
-        Iterate over each layer and stop once the the layer name matches
-        :param model: Model
-        :parm name: string
-        """
-        queue: Queue = Queue()
-        start = model.named_children()
-        queue.put(start)
-        layer_names = []
-        layer_names_str: str = ""
-        while not queue.empty():
-            named_children = queue.get()
-            for layer_name, layer_model in named_children:
-                layer_names.append(layer_name)
-                layer_names_str = ", ".join(layer_names)
-                if layer_name == name:
-                    print(
-                        f"Found layer {layer_name}" "in model layers: {layer_names_str}"
-                    )
-                    return layer_model
-                layer_model._get_name()
-                queue.put(layer_model.named_children())
-        raise GalileoException(
-            f"Layer could not be found in {layer_names_str}, "
-            "make sure to check capitalization"
-        )
-
-    def attach_embedding_hook(
-        self,
-        model: Module,
-        model_layer: Optional[Union[Module, str]] = None,
-        embedding_hook: Callable = print,
-    ) -> RemovableHandle:
-        """Attach hook and save it in our hook list"""
-        if model_layer is None:
-            selected_layer = self.get_embedding_layer_auto(model)
-        elif isinstance(model_layer, str):
-            selected_layer = self.get_embedding_layer_by_name(model, model_layer)
-        else:
-            selected_layer = model_layer
-        return self.attach_hook(selected_layer, embedding_hook)
-
-    def attach_hook(self, selected_layer: Module, hook: Callable) -> RemovableHandle:
-        """Register a hook and save it in our hook list"""
-        h = selected_layer.register_forward_hook(hook)
-        self.hooks.append(h)
-        return h
-
-    def remove_hook(self) -> None:
-        """Remove all hooks from the model"""
-        for h in self.hooks:
-            h.remove()
 
 
 @check_noop
--- tests/test_text_classification_pt.py
+++ tests/test_text_classification_pt.py
@@ -1,64 +1,66 @@
-
-
-import warnings
-warnings.filterwarnings('ignore')
-
-''' Access to the raw dataset iterators '''
 import torch
-from torchtext.datasets import AG_NEWS
-train_iter = iter(AG_NEWS(split='train'))
-
-next(train_iter)
-
-
-''' Prepare data processing pipelines '''
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+from torchtext.data.functional import to_map_style_dataset
 from torchtext.data.utils import get_tokenizer
+from torchtext.datasets import AG_NEWS
 from torchtext.vocab import build_vocab_from_iterator
 
-tokenizer = get_tokenizer('basic_english')
-train_iter = AG_NEWS(split='train')
+train_iter = iter(AG_NEWS(split="train"))
+tokenizer = get_tokenizer("basic_english")
+train_iter = AG_NEWS(split="train")
+
 
 def yield_tokens(data_iter):
     for _, text in data_iter:
         yield tokenizer(text)
 
+
 vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
 vocab.set_default_index(vocab["<unk>"])
 
-vocab(['here', 'is', 'an', 'example'])
+vocab(["here", "is", "an", "example"])
 
-text_pipeline = lambda x: vocab(tokenizer(x))
-label_pipeline = lambda x: int(x) - 1
 
-text_pipeline('here is the an example')
-label_pipeline('10')
+def text_pipeline(x):
+    return vocab(tokenizer(x))
 
-''' Generate data batch and iterator '''
-from torch.utils.data import DataLoader
+
+def label_pipeline(x):
+    return int(x) - 1
+
+
+text_pipeline("here is the an example")
+label_pipeline("10")
+
+""" Generate data batch and iterator """
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def collate_batch(batch):
     label_list, text_list, offsets = [], [], [0]
     for (_label, _text) in batch:
-         label_list.append(label_pipeline(_label))
-         processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
-         text_list.append(processed_text)
-         offsets.append(processed_text.size(0))
+        label_list.append(label_pipeline(_label))
+        processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
+        text_list.append(processed_text)
+        offsets.append(processed_text.size(0))
     label_list = torch.tensor(label_list, dtype=torch.int64)
     offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
     text_list = torch.cat(text_list)
     return label_list.to(device), text_list.to(device), offsets.to(device)
 
-train_iter = AG_NEWS(split='train')
-dataloader = DataLoader(train_iter, batch_size=8, shuffle=False, collate_fn=collate_batch)
 
-next(iter(dataloader))
+train_iter = AG_NEWS(split="train")
+dataloader = DataLoader(
+    train_iter, batch_size=8, shuffle=False, collate_fn=collate_batch
+)
 
-''' Define the model '''
-from torch import nn
+""" Define the model """
+
 
 class TextClassificationModel(nn.Module):
-
     def __init__(self, vocab_size, embed_dim, num_class):
         super(TextClassificationModel, self).__init__()
         self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
@@ -76,21 +78,20 @@ class TextClassificationModel(nn.Module):
         return self.fc(embedded)
 
 
-''' Initate an instance '''
-train_iter = AG_NEWS(split='train')
+""" Initate an instance """
+train_iter = AG_NEWS(split="train")
 num_class = len(set([label for (label, text) in train_iter]))
 vocab_size = len(vocab)
 emsize = 64
 model = TextClassificationModel(vocab_size, emsize, num_class).to(device)
 
-''' Define functions to train the model and evaluate results'''
-import time
+""" Define functions to train the model and evaluate results"""
+
 
 def train(dataloader):
     model.train()
     total_acc, total_count = 0, 0
     log_interval = 500
-    start_time = time.time()
 
     for idx, (label, text, offsets) in enumerate(dataloader):
         optimizer.zero_grad()
@@ -102,12 +103,14 @@ def train(dataloader):
         total_acc += (predicted_label.argmax(1) == label).sum().item()
         total_count += label.size(0)
         if idx % log_interval == 0 and idx > 0:
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches '
-                  '| accuracy {:8.3f}'.format(epoch, idx, len(dataloader),
-                                              total_acc/total_count))
+            print(
+                "| epoch {:3d} | {:5d}/{:5d} batches "
+                "| accuracy {:8.3f}".format(
+                    epoch, idx, len(dataloader), total_acc / total_count
+                )
+            )
             total_acc, total_count = 0, 0
-            start_time = time.time()
+
 
 def evaluate(dataloader):
     model.eval()
@@ -117,17 +120,18 @@ def evaluate(dataloader):
         for idx, (label, text, offsets) in enumerate(dataloader):
             predicted_label = model(text, offsets)
             loss = criterion(predicted_label, label)
+            loss
             total_acc += (predicted_label.argmax(1) == label).sum().item()
             total_count += label.size(0)
-    return total_acc/total_count
+    return total_acc / total_count
 
-'''Split the dataset and run the model'''
-from torch.utils.data.dataset import random_split
-from torchtext.data.functional import to_map_style_dataset
+
+"""Split the dataset and run the model"""
+
 # Hyperparameters
-EPOCHS = 10 # epoch
+EPOCHS = 10  # epoch
 LR = 5  # learning rate
-BATCH_SIZE = 64 # batch size for training
+BATCH_SIZE = 64  # batch size for training
 
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=LR)
@@ -137,36 +141,36 @@ train_iter, test_iter = AG_NEWS()
 train_dataset = to_map_style_dataset(train_iter)
 test_dataset = to_map_style_dataset(test_iter)
 num_train = int(len(train_dataset) * 0.95)
-split_train_, split_valid_ = \
-    random_split(train_dataset, [num_train, len(train_dataset) - num_train])
+split_train_, split_valid_ = random_split(
+    train_dataset, [num_train, len(train_dataset) - num_train]
+)
 
-train_dataloader = DataLoader(split_train_, batch_size=BATCH_SIZE,
-                              shuffle=True, collate_fn=collate_batch)
-valid_dataloader = DataLoader(split_valid_, batch_size=BATCH_SIZE,
-                              shuffle=True, collate_fn=collate_batch)
-test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
-                             shuffle=True, collate_fn=collate_batch)
+train_dataloader = DataLoader(
+    split_train_, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_batch
+)
+valid_dataloader = DataLoader(
+    split_valid_, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_batch
+)
+test_dataloader = DataLoader(
+    test_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_batch
+)
 
 for epoch in range(1, EPOCHS + 1):
-    epoch_start_time = time.time()
     train(train_dataloader)
     accu_val = evaluate(valid_dataloader)
     if total_accu is not None and total_accu > accu_val:
-      scheduler.step()
+        scheduler.step()
     else:
-       total_accu = accu_val
-    print('-' * 59)
-    print('| end of epoch {:3d} | time: {:5.2f}s | '
-          'valid accuracy {:8.3f} '.format(epoch,
-                                           time.time() - epoch_start_time,
-                                           accu_val))
-    print('-' * 59)
+        total_accu = accu_val
+    print("-" * 59)
+    print("| end of epoch {:3d} || " "valid accuracy {:8.3f} ".format(epoch, accu_val))
+    print("-" * 59)
 
 
-''' Evaluate the model with test dataset'''
+""" Evaluate the model with test dataset"""
 
 
 def test_dataset() -> None:
-    print('Checking the results of test dataset.')
+    print("Checking the results of test dataset.")
     accu_test = evaluate(test_dataloader)
-    print('test accuracy {:8.3f}'.format(accu_test))
+    print("test accuracy {:8.3f}".format(accu_test))
