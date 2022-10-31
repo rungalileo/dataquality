2437084a8650e7c6fbd173bd8d3def7b61f1fb53
Franz Gusto <franz@rungalileo.io>
2022-10-31 13:39:25 -0700

    remove pre-commit

--- scripts/pre-commit.sh
+++
@@ -1,3 +0,0 @@
-source .venv/bin/activate
-. ./scripts/set-local-env.sh
-pytest --cov=dataquality --cov=tests --cov=docs_src --cov-report=term-missing --cov-report=xml -o console_output_style=progress --disable-warnings ${@}
