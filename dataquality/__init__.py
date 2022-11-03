d8c62930f57a85a333007b24244da7c246e5be09
Ben Epstein <ben@rungalileo.io>
2022-11-03 09:34:36 -0400

    class label management

--- dataquality/__init__.py
+++ dataquality/__init__.py
@@ -1,6 +1,6 @@
 "dataquality"
 
-__version__ = "v0.7.3a0"
+__version__ = "v0.7.3a1"
 
 import os
 import resource
--- dataquality/auto/text_classification.py
+++ dataquality/auto/text_classification.py
@@ -4,7 +4,8 @@ from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
-from datasets import Dataset, DatasetDict, load_dataset
+from pandas.api.types import is_string_dtype
+from datasets import Dataset, DatasetDict, load_dataset, ClassLabel
 
 import dataquality as dq
 from dataquality.auto.tc_trainer import get_trainer
@@ -23,27 +24,50 @@ DEMO_DATASETS = [
 ]
 
 
-def _get_dataset(data: Union[pd.DataFrame, Dataset, str]) -> Dataset:
+def _process_pandas_df(df: pd.DataFrame, labels: List[str] = None) -> Dataset:
+    label_is_str = is_string_dtype(df["label"]) if "label" in df else False
+    if not label_is_str:
+        return Dataset.from_pandas(df)
+    labels = labels if labels is not None else sorted(set(df["label"].tolist()))
+    label_to_idx = dict(zip(labels, list(range(len(labels)))))
+    df["label"] = df["label"].apply(lambda label: label_to_idx[label])
+    class_label = ClassLabel(num_classes=len(labels), names=labels)
+    ds = Dataset.from_pandas(df)
+    ds = ds.cast_column("label", class_label)
+    return ds
+
+def _add_class_label_to_dataset(ds: Dataset, labels: List[str] = None) -> Dataset:
+    """Map a not ClassLabel 'label' column to a ClassLabel"""
+    if ds.features["label"].dtype == "string":
+        return ds.class_encode_column("label", include_nulls=True)
+    labels = labels if labels is not None else sorted(set(ds["label"]))
+    class_label = ClassLabel(num_classes=len(labels), names=labels)
+    ds = ds.cast_column("label", class_label)
+    return ds
+
+
+def _get_dataset(data: Union[pd.DataFrame, Dataset, str], labels: List[str] = None) -> Dataset:
     """Loads the data into (hf) Dataset format"""
     if isinstance(data, Dataset):
         return data
     if isinstance(data, pd.DataFrame):
-        return Dataset.from_pandas(data)
+        return _process_pandas_df(data, labels)
     if isinstance(data, str):
         ext = os.path.splitext(data)[-1]
         if not ext:
             ds = load_dataset(data)
         else:
-            func = f"from_{ext}"
+            func = f"read_{ext}"
             if not hasattr(Dataset, func):
                 raise GalileoException(
-                    "Local file path extension must be readable by huggingface Dataset."
+                    "Local file path extension must be readable by pandas."
                     f"Found {ext} which is not"
                 )
-            ds = getattr(Dataset, func)(data)
+            df = getattr(pd, func)(data)
+            ds = _process_pandas_df(df, labels)
         assert isinstance(ds, Dataset), (
             f"Loaded data should be of type Dataset, but found {type(ds)}. If ds is a "
-            f"DatasetDict, consider passing it to `hf_data`"
+            f"DatasetDict, consider passing it to `hf_data` (dq.auto(hf_data=data))"
         )
         return ds
     raise GalileoException(
@@ -51,19 +75,21 @@ def _get_dataset(data: Union[pd.DataFrame, Dataset, str]) -> Dataset:
     )
 
 
-def _validate_dataset_dict(dd: DatasetDict) -> DatasetDict:
+def _validate_dataset_dict(dd: DatasetDict, labels: List[str] = None) -> DatasetDict:
     valid_keys = ["train", "training", "test", "testing", "validation"]
     for key in list(dd.keys()):
         assert (
             key in valid_keys
         ), f"All keys of dataset must be one of {valid_keys}. Found {list(dd.keys())}"
-        ds = dd[key]
+        ds = dd.pop(key)
         assert "text" in ds.features, "Dataset must have column `text`"
         assert "label" in ds.features, "Dataset must have column `label`"
         if "id" not in ds.features:
-            dd[key] = ds.add_column("id", list(range(ds.num_rows)))
+            ds = ds.add_column("id", list(range(ds.num_rows)))
+        if not isinstance(ds.features["label"], ClassLabel):
+            ds = _add_class_label_to_dataset(ds, labels)
         # Use the split Enums
-        dd[Split[key]] = dd.pop(key)
+        dd[Split[key]] = ds
     return dd
 
 
@@ -72,6 +98,7 @@ def _get_dataset_dict(
     train_data: Union[pd.DataFrame, Dataset, str] = None,
     val_data: Union[pd.DataFrame, Dataset, str] = None,
     test_data: Union[pd.DataFrame, Dataset, str] = None,
+    labels: List[str] = None
 ) -> DatasetDict:
     dd = DatasetDict()
     if not any([hf_data is not None, train_data is not None]):
@@ -85,12 +112,12 @@ def _get_dataset_dict(
         )
         dd = ds
     else:
-        dd[Split.train] = _get_dataset(train_data)
+        dd[Split.train] = _get_dataset(train_data, labels)
         if val_data is not None:
-            dd[Split.validation] = _get_dataset(val_data)
+            dd[Split.validation] = _get_dataset(val_data, labels)
         if test_data is not None:
-            dd[Split.test] = _get_dataset(test_data)
-    return _validate_dataset_dict(dd)
+            dd[Split.test] = _get_dataset(test_data, labels)
+    return _validate_dataset_dict(dd, labels)
 
 
 def _get_labels(dd: DatasetDict, labels: Optional[List[str]] = None) -> List[str]:
@@ -120,7 +147,7 @@ def auto(
     val_data: Union[pd.DataFrame, Dataset, str] = None,
     test_data: Union[pd.DataFrame, Dataset, str] = None,
     hf_model: str = "distilbert-base-uncased",
-    labels: Optional[List[str]] = None,
+    labels: List[str] = None,
     project_name: str = None,
     run_name: str = None,
     wait: bool = True,
@@ -161,7 +188,7 @@ def auto(
         be generated
     :param wait: Whether to wait for Galileo to complete processing your run
     """
-    dd = _get_dataset_dict(hf_data, train_data, val_data, test_data)
+    dd = _get_dataset_dict(hf_data, train_data, val_data, test_data, labels)
     labels = _get_labels(dd, labels)
     dq.login()
     dq.init(TaskType.text_classification, project_name=project_name, run_name=run_name)
@@ -169,6 +196,9 @@ def auto(
     _log_dataset_dict(dd)
     trainer, encoded_data = get_trainer(dd, labels, hf_model)
     watch(trainer)
+    print("Starting training")
+    print("encoded data")
+    print(encoded_data)
     trainer.train()
     # TODO: What do we do with the test data? Do we call predict here?
     if Split.test in encoded_data:
