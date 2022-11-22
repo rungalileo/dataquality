b221e5a124c068d065cdc0ac405a13b4837406b5
Franz Gusto <franz@rungalileo.io>
2022-11-22 10:21:14 -0500

    added support for test and validation logging

--- dataquality/integrations/experimental/keras.py
+++ dataquality/integrations/experimental/keras.py
@@ -1,6 +1,6 @@
 import inspect
 from functools import partial
-from typing import Any, Dict, List, Tuple, Union
+from typing import Any, Callable, Dict, Optional, Sized, Tuple, Union
 
 import tensorflow as tf
 from keras.engine import data_adapter
@@ -20,19 +20,19 @@ if not is_tf_2():
     tf.compat.v1.enable_eager_execution()
 
 import tensorflow as tf
-from keras.callbacks import Callback
-from keras.layers import Layer
 
 a = Analytics(ApiClient, config)
 a.log_import("integrations/experimental/keras")
 
 
-def pass_on(*args, **kwargs):
+def pass_on(*args: Tuple, **kwargs: Dict[str, Any]) -> None:
     return None
 
 
 # https://github.com/tensorflow/tensorflow/issues/33478#issuecomment-843720638
-def proxy_call(input: tf.Tensor, obj: tf.keras.layers.Layer, *args, **kwargs):
+def proxy_call(
+    input: tf.Tensor, obj: tf.keras.layers.Layer, *args: Tuple, **kwargs: Dict[str, Any]
+) -> Any:
     if obj._before_call is not None:
         obj._before_call(obj, input)
     output = obj._old_call(input)
@@ -43,131 +43,166 @@ def proxy_call(input: tf.Tensor, obj: tf.keras.layers.Layer, *args, **kwargs):
     return output
 
 
-def hook_layer(layer, before_call=None, after_call=None):
+def hook_layer(
+    layer: tf.keras.layers.Layer,
+    before_call: Optional[Callable] = None,
+    after_call: Optional[Callable] = None,
+) -> None:
     layer._before_call = before_call
     layer._after_call = after_call
     layer._old_call = layer.call
     layer.call = partial(proxy_call, obj=layer)
 
 
-def save_input(store, layer: Any, input: Any, *args, **kwargs):
+def save_input(
+    store: Dict[str, Any],
+    layer: tf.keras.layers.Layer,
+    input: tf.Tensor,
+    *args: Tuple,
+    **kwargs: Dict[str, Any],
+) -> None:
     if input is not None:
         store["input"] = input
 
 
 def save_output(
-    store, layer: tf.keras.layers.Layer, input: tf.Tensor, output: tf.Tensor
-):
+    store: Dict[str, Any],
+    layer: tf.keras.layers.Layer,
+    input: tf.Tensor,
+    output: tf.Tensor,
+) -> None:
 
     if output is not None:
         store["output"] = output
-
-
-class ModelHooker(Callback):
-    """
-    ```python
-       callbacks =  tf.keras.callbacks.CallbackList([...])
-       callbacks.append(...)
-       callbacks.on_train_begin(...)
-       for epoch in range(EPOCHS):
-         callbacks.on_epoch_begin(epoch)
-         for i, data in dataset.enumerate():
-           callbacks.on_train_batch_begin(i)
-           batch_logs = model.train_step(data)
-           callbacks.on_train_batch_end(i, batch_logs)
-         epoch_logs = ...
-         callbacks.on_epoch_end(epoch, epoch_logs)
-       final_logs=...
-       callbacks.on_train_end(final_logs)
-    ```
-    """
-
-    if not tf.executing_eagerly():
-        if tf.inside_function():
-            raise ValueError(
-                "This Callback's method contains Python state and "
-                "should be called outside of `tf.function`s."
-            )
-        else:  # Legacy graph mode:
-            raise ValueError(
-                "BackupAndRestore only supports eager mode. In graph "
-                "mode, consider using ModelCheckpoint to manually save "
-                "and restore weights with `model.load_weights()` and by "
-                "providing `initial_epoch` in `model.fit()` for fault "
-                "tolerance."
-            )
-
-    def __init__(self, model, layer, embedding_dims, logits_dims, **kwargs):
-        self.model = model
-        self.index_model = tf.keras.Sequential()
 
 
 class DataQualityCallback(keras.callbacks.Callback):
     helper_data: Any
     model: Any
 
-    def __init__(self, store, model, *args, **kwargs) -> None:
+    def __init__(
+        self,
+        store: Dict[str, Any],
+        model: tf.keras.layers.Layer,
+        *args: Tuple,
+        **kwargs: Dict[str, Any],
+    ) -> None:
         self.helper_data = store
         a.log_function("keras/dqcallback")
         self.model = model
         super(DataQualityCallback, self).__init__()
+        if not tf.executing_eagerly():
+            raise GalileoException("Needs to be executing eagerly")
 
-    def set_params(self, params):
-        self.helper_data["params"] = params
-        self.params = params
-
-    def generate_indices(self, x, kwargs):
+    def generate_test_indices(self, kwargs: Dict[str, Any]) -> None:
         e_model = keras.Sequential([])
         e_model.compile(loss="mse", run_eagerly=True)
-        data_handler = data_adapter.get_data_handler(
-            x=x,
+        data_handler = data_adapter.DataHandler(
+            x=kwargs.get("val_x"),
+            y=None,
+            sample_weight=kwargs.get("val_sample_weight"),
+            batch_size=kwargs.get("validation_batch_size") or kwargs.get("batch_size"),
+            steps_per_epoch=kwargs.get("validation_steps"),
+            initial_epoch=0,
+            epochs=1,
+            max_queue_size=kwargs.get("max_queue_size"),
+            workers=1,
+            use_multiprocessing=False,
             model=e_model,
             steps_per_execution=self.model._steps_per_execution,
-            **{
-                key: value
-                for key, value in kwargs.items()
-                if key
-                not in [
-                    "validation_freq",
-                    "validation_batch_size",
-                    "validation_steps",
-                    "validation_data",
-                    "validation_split",
-                    "callbacks",
-                    "x",
-                    "verbose",
-                ]
-            },
         )
         for epoch, iterator in data_handler.enumerate_epochs():
             for step, iterator_data in enumerate(iterator):
-                self.helper_data[f"epoch_{epoch}_{step}"] = iterator_data
+                self.helper_data[f"val_{step}"] = iterator_data
 
-    def generate_kwargs(self):
-        signature = inspect.signature(self.model.fit)
+    def generate_train_indices(self, kwargs: Dict[str, Any]) -> None:
+        e_model = keras.Sequential([])
+        e_model.compile(loss="mse", run_eagerly=True)
+        filtered_keys = set(kwargs.keys())
+        filtered_keys.discard("model")
+        filtered_keys.discard("steps_per_execution")
+        allowed_keys = set(inspect.signature(data_adapter.DataHandler).parameters)
+
+        keys = filtered_keys.intersection(allowed_keys)
+        dh_kwargs = {key: kwargs.get(key) for key in keys}
+        data_handler = data_adapter.get_data_handler(
+            model=e_model,
+            steps_per_execution=self.model._steps_per_execution,
+            **dh_kwargs,
+        )
+
+        for epoch, iterator in data_handler.enumerate_epochs():
+            for step in data_handler.steps():
+                self.helper_data[f"epoch_{epoch}_{step}"] = next(iterator)
+
+    def generate_kwargs(
+        self, func: Callable, fit_kwargs: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        signature = inspect.signature(func)
         default_params = {
             k: v.default
             for k, v in signature.parameters.items()
             if v.default is not inspect.Parameter.empty
         }
         kwargs = {**default_params}
-        kwargs.update(self.helper_data["fit_kwargs"])
+        kwargs.update(fit_kwargs)
         return kwargs
 
-    def on_train_begin(self, logs=None):
-        self.helper_data["step"] = 0
+    def on_train_begin(self, logs: Any = None) -> None:
         assert self.model.run_eagerly, GalileoException(
             "Model must be compiled with run_eagerly=True"
         )
-        X_values = self.helper_data["fit_kwargs"].get(
-            "x", self.helper_data["fit_args"][0]
-        )
-        x = tf.range(len(X_values))
-        self.generate_indices(x, self.generate_kwargs())
+        fit_kwargs = self.helper_data["model_fit"].get("kwargs")
+        fit_args = self.helper_data["model_fit"].get("args")
+        self.fit_kwargs = self.generate_kwargs(self.model.fit, fit_kwargs)
+        x = self.fit_kwargs.get("x")
+        if x is None and len(fit_args):
+            # TODO: enumerate args and replace kwargs
+            x = fit_args[0]
+        if isinstance(x, Sized):
+            x_len = len(x)
+        else:
+            raise GalileoException("Can not determine length of x")
+        new_kwargs = self.generate_split(self.fit_kwargs, x_len)
+        self.fit_kwargs.update(new_kwargs)
+        self.generate_train_indices(self.fit_kwargs)
+
+    def generate_split(self, kwargs: Dict[str, Any], x_len: int) -> Dict:
+        validation_split = kwargs.get("validation_split")
+        validation_data = kwargs.get("validation_data")
+        sample_weight = kwargs.get("sample_weight")
+        x = tf.range(x_len)
+        y = x
+
+        if validation_split and validation_data is None:
+            # Create the validation data using the training data. Only supported
+            # for `Tensor` and `NumPy` input.
+            (
+                x,
+                y,
+                sample_weight,
+            ), validation_data = data_adapter.train_validation_split(
+                (x, y, sample_weight), validation_split=validation_split
+            )
+
+        if validation_data:
+            (
+                val_x,
+                val_y,
+                val_sample_weight,
+            ) = data_adapter.unpack_x_y_sample_weight(validation_data)
+        val_x = tf.range(len(val_x))
+        return {
+            "x": x,
+            "y": None,
+            "sample_weight": sample_weight,
+            "val_x": val_x,
+            "val_y": None,
+            "val_sample_weight": val_sample_weight,
+        }
 
     def on_epoch_begin(self, epoch: int, logs: Dict) -> None:
         dq.set_epoch(epoch)
-        self.helper_data["step"] = 0
 
     def _clear_logger_config_helper_data(self) -> None:
         self.helper_data.pop("input", None)
@@ -184,9 +219,80 @@ class DataQualityCallback(keras.callbacks.Callback):
         ids = self.helper_data.get("indices_ids")
         if ids is None:
             epoch = logger_config.cur_epoch
-            step = self.helper_data["step"]
+            step = batch
             ids = self.helper_data[f"epoch_{epoch}_{step}"]
-            self.helper_data["step"] += 1
+        dq.log_model_outputs(
+            embs=self.helper_data["input"],
+            logits=self.helper_data["output"],
+            ids=ids,
+        )
+
+    def on_test_begin(self, logs: Any = None) -> None:
+        dq.set_split(Split.test)
+        self.generate_test_indices(self.fit_kwargs)
+
+    def on_test_batch_begin(self, batch: Any, logs: Dict = None) -> None:
+        # TODO: Somehow we should figure out whether this is in .fit
+        #  (so really this should be val) or .evaluate (so this should be test)
+        self._clear_logger_config_helper_data()
+        dq.set_split(Split.validation)
+
+    def on_test_batch_end(self, batch: Any, logs: Dict = None) -> None:
+        dq.set_split(Split.validation)
+        # logger_config = dq.get_data_logger().logger_config
+        ids = self.helper_data.get("indices_ids")
+
+        if ids is None:
+            step = batch
+            ids = self.helper_data[f"val_{step}"]
+        dq.log_model_outputs(
+            embs=self.helper_data["input"],
+            logits=self.helper_data["output"],
+            ids=ids,
+        )
+
+    def on_predict_begin(self, batch: int) -> None:
+        dq.set_split(Split.test)
+        predict_kwargs = self.helper_data["model_predict"].get("kwargs")
+        predict_args = self.helper_data["model_predict"].get("args")
+        self.predict_kwargs = self.generate_kwargs(self.model.predict, predict_kwargs)
+        x = self.predict_kwargs.get("x")
+        if x is None and len(predict_args):
+            # TODO: enumerate args and replace kwargs
+            x = predict_args[0]
+        if isinstance(x, Sized):
+            x_len = len(x)
+        else:
+            raise GalileoException("Can not determine length of x")
+        e_model = keras.Sequential([])
+        e_model.compile(loss="mse", run_eagerly=True)
+        data_handler = data_adapter.DataHandler(
+            x=tf.range(x_len),
+            batch_size=self.predict_kwargs.get("batch_size"),
+            steps_per_epoch=self.predict_kwargs.get("steps"),
+            initial_epoch=0,
+            epochs=1,
+            max_queue_size=self.predict_kwargs.get("max_queue_size"),
+            workers=1,
+            use_multiprocessing=False,
+            model=e_model,
+            steps_per_execution=self.model._steps_per_execution,
+        )
+        for epoch, iterator in data_handler.enumerate_epochs():
+            for step, iterator_data in enumerate(iterator):
+                self.helper_data[f"test_{step}"] = iterator_data
+
+    def on_predict_batch_begin(self, batch: int, logs: Any = None) -> None:
+        dq.set_epoch(0)
+
+    def on_predict_batch_end(self, batch: int, logs: Any = None) -> None:
+        dq.set_split(Split.test)
+        # logger_config = dq.get_data_logger().logger_config
+        ids = self.helper_data.get("indices_ids")
+
+        if ids is None:
+            step = batch
+            ids = self.helper_data[f"test_{step}"]
 
         dq.log_model_outputs(
             embs=self.helper_data["input"],
@@ -194,36 +300,12 @@ class DataQualityCallback(keras.callbacks.Callback):
             ids=ids,
         )
 
-    def on_test_begin(self, logs=None):
-        dq.set_split(Split.test)
-        self.helper_data["step"] = 0
 
-    def on_test_batch_begin(self, batch: Any, logs: Dict = None) -> None:
-        # TODO: Somehow we should figure out whether this is in .fit
-        #  (so really this should be val) or .evaluate (so this should be test)
-        self._clear_logger_config_helper_data()
-        dq.set_split(Split.test)
-
-    def on_test_batch_end(self, batch: Any, logs: Dict = None) -> None:
-        dq.set_split(Split.test)
-        logger_config = dq.get_data_logger().logger_config
-        ids = self.helper_data.get("indices_ids")
-        return
-        if ids is None:
-            ids = self.helper_data[f"epoch_{logger_config.cur_epoch}"]
-
-        dq.log_model_outputs(
-            embs=self.helper_data["input"],
-            logits=self.helper_data["output"][:, : len(logger_config.labels)],
-            ids=ids,
-        )
-
-
-def store_model_args_kwargs(store, callback):
-    def fit_wrapper(orig_func, *args, **kwargs):
+def store_args_kwargs(store: Dict[str, Any], callback: Callable) -> Callable:
+    def fit_wrapper(orig_func: Callable, *args: Any, **kwargs: Any) -> None:
         """Stores the indices of the batch"""
-        store["fit_args"] = args
-        store["fit_kwargs"] = kwargs
+        store["args"] = args
+        store["kwargs"] = kwargs
         if kwargs.get("callbacks"):
             kwargs["callbacks"].append(callback)
         else:
@@ -233,8 +315,8 @@ def store_model_args_kwargs(store, callback):
     return fit_wrapper
 
 
-def store_model_ids(store):
-    def train_step_wrapper(orig_func, *args, **kwargs):
+def store_model_ids(store: Dict[str, Any]) -> Callable:
+    def train_step_wrapper(orig_func: Callable, *args: Any, **kwargs: Any) -> None:
         """Stores the indices of the batch"""
         try:
             store["indices_ids"] = args[0][0].pop("id", None)
@@ -246,7 +328,9 @@ def store_model_ids(store):
     return train_step_wrapper
 
 
-def select_model_layer(model, layer):
+def select_model_layer(
+    model: tf.keras.layers.Layer, layer: Optional[Union[tf.keras.layers.Layer, str]]
+) -> tf.keras.layers.Layer:
     chosen_layer = layer
     if isinstance(chosen_layer, str) is None:
         for model_layer in model.layers:
@@ -258,14 +342,36 @@ def select_model_layer(model, layer):
             if model_layer.name == "classifier":
                 chosen_layer = model_layer
                 break
+    assert chosen_layer is not None, GalileoException("Layer could not be found")
+    assert not isinstance(chosen_layer, str), GalileoException(
+        "Layer could not be found"
+    )
     return chosen_layer
 
 
-def watch(model: tf.keras.layers.Layer, layer=None, seed=42):
+def watch(model: tf.keras.layers.Layer, layer: Any = None, seed: int = 42) -> None:
     tf.random.set_seed(seed)
     logger_data = dq.get_model_logger().logger_config.helper_data
     callback = DataQualityCallback(logger_data, model)
-    model.fit = wrap_fn(model.fit, store_model_args_kwargs(logger_data, callback))
+    logger_data["model_fit"] = {}
+
+    logger_data["model_patches"] = {
+        "predict": model.predict,
+        "test_step": model.test_step,
+        "predict_step": model.predict_step,
+        "__call__": model.__call__,
+        "fit": model.fit,
+    }
+
+    model.fit = wrap_fn(
+        model.fit, store_args_kwargs(logger_data["model_fit"], callback)
+    )
+    logger_data["model_predict"] = {}
+
+    model.predict = wrap_fn(
+        model.predict, store_args_kwargs(logger_data["model_predict"], callback)
+    )
+
     model.train_step = wrap_fn(model.train_step, store_model_ids(logger_data))
     model.test_step = wrap_fn(model.test_step, store_model_ids(logger_data))
     model.predict_step = wrap_fn(model.predict_step, store_model_ids(logger_data))
@@ -276,3 +382,17 @@ def watch(model: tf.keras.layers.Layer, layer=None, seed=42):
         before_call=partial(save_input, logger_data),
         after_call=partial(save_output, logger_data),
     )
+
+
+def unwatch(model: tf.keras.layers.Layer) -> None:
+    logger_data = dq.get_model_logger().logger_config.helper_data
+    for k, v in logger_data.get("model_patches", {}).items():
+        print("unpatching", k)
+        setattr(model, k, v)
+
+    for layer in model.layers:
+        if hasattr(layer, "_old_call"):
+            layer.call = layer._old_call
+            del layer._old_call
+            del layer._before_call
+            del layer._after_call
--- tests/integrations/keras/test_experimental.py
+++ tests/integrations/keras/test_experimental.py
@@ -1,30 +1,30 @@
-from typing import Any, Callable, Generator
-import vaex
+from typing import Callable, Generator
 from unittest.mock import MagicMock, patch
+
+import pandas as pd
+import tensorflow as tf
+import vaex
+from keras.engine import data_adapter
 from transformers import (
-    TFAutoModelForSequenceClassification,
     AutoTokenizer,
     DataCollatorWithPadding,
+    TFAutoModelForSequenceClassification,
 )
-import tensorflow as tf
+
 import dataquality as dq
-from dataquality import config
-from dataquality.integrations.experimental.keras import watch
+from dataquality.integrations.experimental.keras import unwatch, watch
 from dataquality.schemas.task_type import TaskType
 from dataquality.utils.thread_pool import ThreadPoolManager
+from tests.conftest import LOCATION
 
 # from tests.conftest import LOCATION
 from tests.test_utils.hf_datasets_mock import mock_dataset, mock_dataset_numbered
-from tests.test_utils.mock_request import (
-    mocked_create_project_run,
-    mocked_get_project_run,
-)
-from tests.conftest import LOCATION
 
 tmp_checkpoint = "tmp/tiny-distillbert"
 checkpoint = "hf-internal-testing/tiny-bert-for-token-classification"
 checkpoint = "distilbert-base-uncased"
 # Load models locally
+
 try:
     tokenizer = AutoTokenizer.from_pretrained(tmp_checkpoint)
 except Exception:
@@ -44,7 +44,6 @@ def preprocess_function(examples, tokenizer):
 
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="tf")
-
 
 # 🔭🌕 Galileo logging
 mock_dataset_with_ids = mock_dataset.map(lambda x, idx: {"id": idx}, with_indices=True)
@@ -77,7 +76,7 @@ batch_size = 4
 @patch.object(dq.core.finish, "upload_dq_log_file")
 @patch.object(dq.clients.api.ApiClient, "make_request")
 @patch.object(dq.core.finish, "wait_for_run")
-def test_tf_watch_e2e_numbered(
+def test_hf_watch_e2e_numbered(
     mock_wait_for_run: MagicMock,
     mock_make_request: MagicMock,
     mock_upload_log_file: MagicMock,
@@ -133,8 +132,123 @@ def test_tf_watch_e2e_numbered(
 
     ThreadPoolManager.wait_for_threads()
     # All data for splits should be logged
-    assert len(vaex.open(f"{LOCATION}/training/0/*.hdf5")) == len(train_dataset)
-    assert len(vaex.open(f"{LOCATION}/validation/0/*.hdf5")) == len(val_dataset)
-    assert len(vaex.open(f"{LOCATION}/test/0/*.hdf5")) == len(test_dataset)
+    assert len(vaex.open(f"{LOCATION}/training/0/*.hdf5")) == len(
+        encoded_train_dataset_number
+    )
+    assert len(vaex.open(f"{LOCATION}/validation/0/*.hdf5")) == len(
+        encoded_test_dataset_number
+    )
     # Should upload without failing on data validation or otherwise
     dq.finish()
+
+
+@patch.object(dq.core.init.ApiClient, "valid_current_user", return_value=True)
+@patch.object(dq.core.finish, "_version_check")
+@patch.object(dq.core.finish, "_reset_run")
+@patch.object(dq.core.finish, "upload_dq_log_file")
+@patch.object(dq.clients.api.ApiClient, "make_request")
+@patch.object(dq.core.finish, "wait_for_run")
+def test_tf_watch_e2e_numbered(
+    mock_wait_for_run: MagicMock,
+    mock_make_request: MagicMock,
+    mock_upload_log_file: MagicMock,
+    mock_reset_run: MagicMock,
+    mock_version_check: MagicMock,
+    mock_valid_user: MagicMock,
+    set_test_config: Callable,
+    cleanup_after_use: Generator,
+) -> None:
+    set_test_config(task_type=TaskType.text_classification)
+    dataset_len = 13
+    val_dataset_len = 14
+    # 🔭🌕 Galileo logging
+    dq.set_labels_for_run(tf.range(10).numpy())
+    train_dataset = pd.DataFrame(
+        {
+            "text": ["x"] * dataset_len,
+            "label": [1] * dataset_len,
+            "id": tf.range(dataset_len).numpy(),
+        }
+    )
+    val_dataset = pd.DataFrame(
+        {
+            "text": ["x"] * val_dataset_len,
+            "label": [1] * val_dataset_len,
+            "id": tf.range(val_dataset_len).numpy(),
+        }
+    )
+    dq.log_dataset(train_dataset, split="train")
+    dq.log_dataset(val_dataset, split="validation")
+    dq.log_dataset(val_dataset, split="test")
+
+    batch_size = 4
+    input_data = (dataset_len, 28, 28, 1)
+    input_shape = (28, 28, 1)
+    num_classes = 10
+    # reuters_mlp_benchmark_test.py
+    # mnist_conv_benchmark_test.py
+    model_s = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(num_classes, activation="softmax", name="classifier"),
+        ]
+    )
+    model_s.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+        run_eagerly=True,
+    )
+    x = tf.ones(input_data)
+    y_ = tf.ones(dataset_len, dtype="int32")
+    y = tf.one_hot(y_, depth=10)
+    val_input_data = (val_dataset_len, 28, 28, 1)
+    val_x = tf.ones(val_input_data)
+    val_y_ = tf.ones(val_dataset_len, dtype="int32")
+    val_y = tf.one_hot(val_y_, depth=10)
+    watch(model_s, "classifier")
+    model_s.fit(
+        x=x,
+        y=y,
+        validation_data=(val_x, val_y),
+        batch_size=batch_size,
+        epochs=2,
+    )
+
+    model_s.predict(x=x, batch_size=batch_size)
+
+    ThreadPoolManager.wait_for_threads()
+    assert len(vaex.open(f"{LOCATION}/training/0/*.hdf5")) == len(x)
+    assert len(vaex.open(f"{LOCATION}/validation/0/*.hdf5")) == len(val_x)
+    assert len(vaex.open(f"{LOCATION}/test/0/*.hdf5")) == len(x)
+    print("finishing")
+    unwatch(model_s)
+    dq.finish()
+    model_s.fit(
+        x=x,
+        y=y,
+        validation_data=(val_x, val_y),
+        batch_size=batch_size,
+        epochs=2,
+    )
+
+
+def test_create_epoch_data() -> None:
+    dh_kwargs = {"x": tf.range(13), "batch_size": 4, "epochs": 2}
+    e_model = tf.keras.Sequential([])
+    e_model.compile(loss="mse", run_eagerly=True)
+    data_handler = data_adapter.DataHandler(
+        model=e_model,
+        **dh_kwargs,
+    )
+    for epoch, iterator in data_handler.enumerate_epochs():
+        for step in data_handler.steps():
+            assert isinstance(epoch, int)
+            assert isinstance(step, int)
+            assert len(next(iterator))
