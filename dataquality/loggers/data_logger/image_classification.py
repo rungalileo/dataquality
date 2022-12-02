4dbf1f0f7d7eb753827a41f47b6ef1ce726b8d16
Anthony Corletti <anthony@rungalileo.io>
2022-12-02 15:59:40 -0500

    wip

--- dataquality/loggers/data_logger/image_classification.py
+++ dataquality/loggers/data_logger/image_classification.py
@@ -1,6 +1,10 @@
-from typing import List
+import os
+from typing import List, Optional, Union
 
-from dataquality.loggers.data_logger.base_data_logger import MetasType
+import pandas as pd
+from PIL import Image
+
+from dataquality.loggers.data_logger.base_data_logger import ITER_CHUNK_SIZE, MetasType
 from dataquality.loggers.data_logger.text_classification import (
     TextClassificationDataLogger,
 )
@@ -8,6 +12,8 @@ from dataquality.loggers.logger_config.image_classification import (
     ImageClassificationLoggerConfig,
     image_classification_logger_config,
 )
+from dataquality.schemas.split import Split
+from dataquality.utils.cv import _img_to_b64_str
 
 
 class ImageClassificationDataLogger(TextClassificationDataLogger):
@@ -34,6 +40,28 @@ class ImageClassificationDataLogger(TextClassificationDataLogger):
             inference_name=inference_name,
         )
 
-    def update_logged_dataset_text(self) -> None:
-        # TODO
-        pass
+    def log_image_dataset(
+        self,
+        dataset: pd.DataFrame,
+        imgs_dir: str,
+        *,
+        imgs_location_colname: Optional[str] = "relpath",
+        batch_size: int = ITER_CHUNK_SIZE,
+        text: Union[str, int] = "text",
+        id: Union[str, int] = "id",
+        label: Optional[Union[str, int]] = "label",
+        split: Optional[Split] = None,
+        meta: Optional[List[Union[str, int]]] = None,
+    ) -> None:
+        dataset["text"] = dataset[imgs_location_colname].apply(
+            lambda x: _img_to_b64_str(img=Image.open(os.path.join(imgs_dir, x)))
+        )
+        self.log_dataset(
+            dataset=dataset,
+            batch_size=batch_size,
+            text=text,
+            id=id,
+            label=label,
+            split=split,
+            meta=meta,
+        )
