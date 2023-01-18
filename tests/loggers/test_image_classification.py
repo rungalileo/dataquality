039f463a1b124e50d05960190c541d5bdb7d312d
Robert Friel <rob@rungalileo.io>
2023-01-18 14:19:25 -0800

    add test

--- tests/loggers/test_image_classification.py
+++ tests/loggers/test_image_classification.py
@@ -1,14 +1,67 @@
+import os.path
 from unittest import mock
 from unittest.mock import MagicMock
+from tempfile import TemporaryDirectory
+from io import BytesIO
+import base64
 
 import numpy as np
+import pandas as pd
 import vaex
+from PIL import Image
 
 import dataquality
 import dataquality as dq
 from dataquality.utils.thread_pool import ThreadPoolManager
 from dataquality.utils.vaex import validate_unique_ids
 from tests.conftest import LOCATION
+
+
+def test_base64(set_test_config, cleanup_after_use) -> None:
+    """
+    TODO
+    """
+    set_test_config(task_type="image_classification")
+
+    def make_img(w, h):
+        a = np.random.randint(256, size=(w, h, 3), dtype=np.uint8)
+        return Image.fromarray(a)
+
+    with TemporaryDirectory() as imgs_dir:
+        images = []
+        image_paths = []
+        labels = []
+        ids = []
+        for i, xtn in enumerate(['.jpg', '.png', '.jpeg', '.gif']):
+            image_path = os.path.join(imgs_dir, f"{i:.03d}.{xtn}")
+            image = make_img(32, 32).save(image_path)
+
+            images.append(image)
+            image_paths.append(image_path)
+            labels.append("A" if i % 2 == 0 else "B")
+            ids.append(i)
+
+        dataset = pd.DataFrame(
+            dict(id=ids, label=labels, path=image_paths,)
+        )
+
+        dq.set_labels_for_run(["A", "B"])
+        dq.log_image_dataset(
+            dataset=dataset,
+            label="label",
+            imgs_location_colname="path"
+        )
+        ThreadPoolManager.wait_for_threads()
+        df = vaex.open(f"{LOCATION}/input_data/training/data_0.arrow")
+
+        base64_images = df["text"]
+        assert len(base64_images) == len(images)
+
+        for image, base64_image in zip(images, base64_images):
+            _, _, content = base64_image.partition("base64,")
+            assert len(content) > 0
+            decoded = Image.open(BytesIO(base64.b64decode(content)))
+            assert np.allclose(np.array(image), np.array(decoded))
 
 
 def test_duplicate_ids_augmented_loop_thread(
