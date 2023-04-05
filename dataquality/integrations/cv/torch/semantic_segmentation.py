a96a50a85dfc124c2c1f59cf530ccd5dac799e90
Derek <dcaustin33@gmail.com>
2023-04-05 13:33:08 -0700

    Added typing for sem_seg

--- dataquality/integrations/cv/torch/semantic_segmentation.py
+++ dataquality/integrations/cv/torch/semantic_segmentation.py
@@ -1,10 +1,11 @@
-from typing import Any, List, Optional
+from typing import Any, Callable, Dict, List, Tuple, Union
+
 
 import torch
 from torch.utils.data import DataLoader
-from torch.nn import Module
-from dataquality.utils.helpers import map_indices_to_ids, wrap_fn
-
+import numpy as np
+from dataquality.utils.helpers import wrap_fn
+from dataquality.utils.torch import store_batch_indices
 from dataquality.loggers.logger_config.semantic_segmentation import (
     semantic_segmentation_logger_config,
 )
@@ -12,38 +13,44 @@ from dataquality.loggers.model_logger.semantic_segmentation import (
     SemanticSegmentationModelLogger,
 )
 from dataquality.utils.cv.semantic_segmentation.utils import mask_to_boundary
-from dataquality.utils.torch import (
-    patch_dataloaders,
-    patch_iterator_with_store,
-)
-from dataquality.integrations.torch import (
-    TorchLogger
-)
-from dataquality.schemas.torch import HelperData
 
 
 class StoreHook:
     def on_finish(*args: Any, **kwargs: Any) -> None:
         pass
 
-    def hook(self, model, model_input, model_output) -> None:
+    def hook(self, 
+             model: torch.nn.Module, 
+             model_input: torch.Tensor, 
+             model_output: Dict[str, torch.Tensor]) -> None:
+        """"
+        Hook to store the model input (tensor) and extract the output from a dictionary and store
+
+        :param model: torch.nn.Module segmentation model
+        :param model_input: torch.Tensor input to the model - an image (bs, 3, h, w)
+        :param model_output: torch.Tensor output of the model - a mask same dim as input (bs, h, w)
+        """
         self.model = model
         self.model_input = model_input
+        # model_output['out'] is common for torch segmentation models as they use resizing and return a dict
+        # will have to adjust for transformer models / check if output is a dict
         self.model_output = model_output["out"]
         self.on_finish(model_input, model_output)
 
 
-class BatchLogger:
-    def __call__(self, batch) -> Any:
-        self.batch = batch
-        return self.batch
-
-
 class Manager:
-    def __init__(self, model, num_classes: int = 10) -> None:
+    """"
+    Hook manager to log the necessary data for semantic segmentation
+    Contains preprocessing to convert output to a format that can be logged
+    """
+    def __init__(self, model: torch.nn.Module, num_classes: int = 10) -> None:
+        """
+        :param model: torch.nn.Module segmentation model
+        :param num_classes: int number of classes in the model (possible we can extract)
+        """
         self.step_pred = StoreHook()
         self.step_pred.h = model.register_forward_hook(self.step_pred.hook)
-        self.bl = {}
+        self.bl = {} # So this will now have batch and ids.
         self.batch_idx = {}
         self.hooked = True
         self.step_pred.on_finish = self._after_pred_step
@@ -52,7 +59,11 @@ class Manager:
         self.helper_data = {}
 
     def _after_pred_step(self, *args: Any, **kwargs: Any) -> None:
-        # import pdb; pdb.set_trace()
+        """
+        Method called after each prediction step by the model 
+        Then processes the output along with the mask to log the appropriate
+        predictions and masks that can be used to calculate data quality metrics
+        """
         with torch.no_grad():
             logging_data = self.bl['batch']
             preds = self.step_pred.model_output
@@ -92,17 +103,20 @@ class Manager:
             # logger._get_data_dict()
             logger.log()
 
-    def register_hooks(self, model) -> None:
-        self.step_embs.h = model.register_forward_hook(self.step_embs.hook)
-
-from typing import Any, Callable, Dict, List, Tuple
 
 # store the batch
-def store_batch(store: Dict[str, List[int]]) -> Callable:
+def store_batch(store: Dict[str, Dict[str, Union[np.ndarray, torch.Tensor]]]) -> Callable:
+    """
+    Stores the batch in the passed store
+    :param store: Dict[str, torch.Tensor] location to store the batch
+    """
     def process_batch(
         next_batch_func: Callable, *args: Tuple, **kwargs: Dict[str, Any]
     ) -> Callable:
-        """Stores the batch"""
+        """
+        Patches the next_batch function to store the batch as well as returning
+        :param next_batch_func: Callable original next_batch function of the dataloader
+        """
         batch = next_batch_func(*args, **kwargs)
         if batch:
             store['batch'] = batch
@@ -112,31 +126,37 @@ def store_batch(store: Dict[str, List[int]]) -> Callable:
 
 
 # add patch to the dataloader iterator
-def patch_iterator_with_batch_store(store: Dict[str, List[int]]) -> Callable:
-    """Patches the iterator of the dataloader to return the indices"""
+def patch_iterator_and_batch(store: Dict[str, Any]) -> Callable:
+    """
+    Patches the iterator of the dataloader to return the indices and the batch
+    :param store: Dict[str, Any] location to store the indices and the batch
+    """
 
     def patch_iterator(
         orig_iterator: Callable, *args: Tuple, **kwargs: Dict[str, Any]
     ) -> Callable:
+        """
+        Patches the iterator and wraps the next_index and next_data methods
+        :param orig_iterator: Callable original iterator of the dataloader
+        """
         iteraror = orig_iterator(*args, **kwargs)
+        iteraror._next_index = wrap_fn(iteraror._next_index, store_batch_indices(store))
         iteraror._next_data = wrap_fn(iteraror._next_data, store_batch(store))
         return iteraror
 
     return patch_iterator
 
+
 def watch(model: Any, dataloader: DataLoader, n_classes: int) -> None:
     """
     Watches a model and logs the model outputs to the Galileo server
     :param model: Model to watch
-    :return:
     """
     tl = Manager(model, num_classes=n_classes)
     semantic_segmentation_logger_config.helper_data["manager"] = tl
-
     dataloader._get_iterator = wrap_fn(  # type: ignore
                 dataloader._get_iterator,
-                patch_iterator_with_batch_store(
+                patch_iterator_and_batch(
                     tl.bl
                 ),
-            )
-    
+            )    
