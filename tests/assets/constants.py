75e32f8dea756283c9851e7263bee8dd7df1fd6f
Bogdan Gheorghe <bogdan@rungalileo.io>
2023-04-25 10:25:29 -0400

    add test

---
+++ tests/assets/constants.py
@@ -0,0 +1,3 @@
+import os
+
+TEST_ASSETS_DIR = f"{os.getcwd()}/tests/assets"
--- tests/utils/test_vaex_utils.py
+++ tests/utils/test_vaex_utils.py
@@ -1,6 +1,9 @@
+import numpy as np
 import vaex
 
-from dataquality.utils.vaex import drop_empty_columns
+from dataquality.utils.cuda import PCA_N_COMPONENTS
+from dataquality.utils.vaex import add_pca_to_df, drop_empty_columns
+from tests.assets.constants import TEST_ASSETS_DIR
 
 
 def test_drop_empty_cols() -> None:
@@ -14,3 +17,14 @@ def test_drop_empty_cols() -> None:
     assert split_df["z"].ismissing().to_numpy().all()
     split_df = drop_empty_columns(split_df)
     assert "z" not in split_df.get_column_names()
+
+
+def test_add_pca_to_df() -> None:
+    embs = np.load(f"{TEST_ASSETS_DIR}/embs.npy")
+    df = vaex.from_arrays(x=[1, 2, 3], y=[4, 5, 6], z=[7, 8, 9], emb=embs)
+    df_copy = add_pca_to_df(df)
+
+    pca_embs = df_copy["emb_pca"].values
+    assert pca_embs.shape == (3, PCA_N_COMPONENTS)
+    assert np.any(pca_embs[:, :3])
+    assert not np.any(pca_embs[:, 3:])
