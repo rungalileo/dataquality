41f7a38d44604c5e9e103648c4d449aa72e65913
Bogdan Gheorghe <bogdan@rungalileo.io>
2023-04-26 15:40:59 -0400

    Address comments

--- tests/utils/test_vaex_utils.py
+++ tests/utils/test_vaex_utils.py
@@ -19,7 +19,7 @@ def test_drop_empty_cols() -> None:
     assert "z" not in split_df.get_column_names()
 
 
-def test_add_pca_to_df() -> None:
+def test_add_pca_to_df_with_padding() -> None:
     np.random.seed(NUMPY_SEED)
     # DETR returns embeddings of dimension 2048.
     df = vaex.from_arrays(
@@ -28,6 +28,9 @@ def test_add_pca_to_df() -> None:
     df = add_pca_to_df(df)
 
     pca_embs = df["emb_pca"].values
+    # Test that padding worked and the embeddings has the correct dim
     assert pca_embs.shape == (3, PCA_N_COMPONENTS)
+    # Test that the embeddings in dim 0,1,2 are not all zeros
     assert np.any(pca_embs[:, :3])
+    # Test that the embeddings in dim > 2 are all zeros (the padding)
     assert not np.any(pca_embs[:, 3:])
