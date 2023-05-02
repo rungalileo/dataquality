d3133d927e2ec1b3a40ae350208e0ff14f168878
Anthony Corletti <anthony@rungalileo.io>
2023-05-02 12:16:45 -0400

    0.8.35a0

--- dataquality/__init__.py
+++ dataquality/__init__.py
@@ -30,7 +30,7 @@ If you want to train without a model, you can use the auto framework:
         dataquality.get_insights()
 """
 
-__version__ = "v0.8.34"
+__version__ = "v0.8.35a0"
 
 import sys
 from typing import Any, List, Optional
