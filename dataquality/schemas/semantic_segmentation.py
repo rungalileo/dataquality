1af3f78f05e75d81266d18bdf1cdf5ce891c528f
Elliott <elliott@rungalileo.io>
2023-05-04 17:09:14 -0400

    Feature: semantic segmentation (#547)
    PR to implement semantic segmentation in dq. Defines error types and upload necessary

--- dataquality/schemas/semantic_segmentation.py
+++ dataquality/schemas/semantic_segmentation.py
@@ -1,4 +1,8 @@
 from enum import Enum
+from typing import Dict, List
+
+import numpy as np
+from pydantic import BaseModel
 
 
 class SemSegCols(str, Enum):
@@ -8,3 +12,79 @@ class SemSegCols(str, Enum):
     # mixin restriction on str (due to "str".split(...))
     split = "split"  # type: ignore
     meta = "meta"
+
+
+class ErrorType(str, Enum):
+    classification = "classification"
+    undetected = "undetected"
+    none = None
+
+
+class Pixel(BaseModel):
+    x: int
+    y: int
+
+    def deserialize(self) -> List[List[int]]:
+        """Takes a pixel object and returns JSON compatible list
+
+        We deserialize to a JSON compatible format that matches what OpenCV
+        expects when drawing contours.
+
+        OpenCV expects a list of list of pixel coordinates.
+        """
+        return [[self.x, self.y]]
+
+
+class Contour(BaseModel):
+    pixels: List[Pixel]
+
+
+class Polygon(BaseModel):
+    id: int
+    label_idx: int
+    error_type: ErrorType = ErrorType.none
+    contours: List[Contour]
+
+    def deserialize_opencv(self) -> List[np.ndarray]:
+        """Deserialize the contours in a polygon to be OpenCV contour compatible
+
+        OpenCV.drawContours expects a list of np.ndarrays corresponding
+        to the contours in the polygon.
+
+        Example:
+            polygon = Polygon(
+                contours=[Contour(pixels=[Pixel(x=0, y=0), Pixel(x=0, y=1)])]
+            )
+            polygon.deserialize_opencv()
+            >>> [np.array([[0, 0], [0, 1]])]
+        """
+        contours = []
+        for contour in self.contours:
+            pixels = [pixel.deserialize() for pixel in contour.pixels]
+            contours.append(np.array(pixels))
+        return contours
+
+    def deserialize_json(self) -> Dict:
+        """Deserialize a polygon object to be JSON compatible for Minio upload
+
+        We export polygons as a nested list of pixels for the Frontend to draw. This
+        nested list format is required by the OpenCV library to draw contours.
+
+        Example:
+            polygon = Polygon(
+                contours=[Contour(pixels=[Pixel(x=0, y=0), Pixel(x=0, y=1)])]
+            )
+            polygon.deserialize_json()
+            >>> [[[[[0, 0]], [[0, 1]]]]]
+        """
+        contours = []
+        for contour in self.contours:
+            pixels = [pixel.deserialize() for pixel in contour.pixels]
+            contours.append([pixels])
+
+        return {
+            "id": self.id,
+            "label_idx": self.label_idx,
+            "error_type": self.error_type.value,
+            "polygon": contours,
+        }
