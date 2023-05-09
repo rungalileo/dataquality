ecbefd26b771f0fe1e340c2e123412064652ee8a
Franz Gusto <franz@rungalileo.io>
2023-05-09 10:49:19 -1000

    added trainer watcher

--- dataquality/integrations/setfit.py
+++ dataquality/integrations/setfit.py
@@ -1,10 +1,13 @@
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
 
 import dataquality as dq
 from dataquality.schemas.split import Split
 from dataquality.utils.cleanup import Cleanup, RefManager
+
+if TYPE_CHECKING:
+    from setfit import SetFitModel, SetFitTrainer
 
 
 class SetFitModelHook:
@@ -28,7 +31,7 @@ class SetFitModelHook:
         else:
             self.store = {}
 
-        self.func_name = "model_head"
+        self.cls_name = "model_head"
         self.func_name_predict = func_name
         self.n_labels = n_labels
         self.setup()
@@ -36,9 +39,13 @@ class SetFitModelHook:
     def setup(self) -> None:
         """Setup hook to SetFit model by replacing predict_proba function with self."""
 
-        self.old_model = getattr(self.in_cls, self.func_name)
+        self.old_model = getattr(self.in_cls, self.cls_name)
         self.old_func = getattr(self.old_model, self.func_name_predict)
         setattr(self.old_model, self.func_name_predict, self)
+        unpatch = _PatchSetFitModel(
+            self.in_cls,
+        )
+        unpatch.model_unpatch = self.unpatch
         self.store["hook"] = self
 
     def __call__(self, *args: Tuple, **kwargs: Dict) -> Any:
@@ -49,7 +56,7 @@ class SetFitModelHook:
         self.store["input_args"] = args
         self.store["input_kwargs"] = kwargs
         output = self.old_func(*args, **kwargs)
-        if self.func_name == "predict":
+        if self.cls_name == "predict":
             assert self.n_labels is not None, "n_labels must be set"
             self.store["output"] = np.eye(self.n_labels)[output]
         self.store["output"] = output
@@ -61,7 +68,107 @@ class SetFitModelHook:
         setattr(self.old_model, self.func_name_predict, self.old_func)
 
 
-def watch(model: Any) -> Callable:
+class _PatchSetFitModel:
+    def __init__(
+        self, setfit_model: SetFitModel, function_name: str = "save_pretrained"
+    ) -> None:
+        self.model = setfit_model
+        self.old_fn = getattr(self.model, function_name)
+        setattr(self.model, function_name, self)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        self.unpatch()
+        self.model_unpatch()
+        return self.old_fn(*args, **kwds)
+
+    def unpatch(self) -> None:
+        setattr(self.model, self.old_fn)
+
+
+class _PatchSetFitTrainer:
+    def __init__(
+        self, setfit_trainer: SetFitTrainer, function_name: str = "train"
+    ) -> None:
+        self.trainer = setfit_trainer
+        self.old_fn = getattr(self.trainer, function_name)
+        setattr(self.trainer, function_name, self)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        batch_size = kwds.get("batch_size", self.trainer.batch_size)
+        if batch_size is not None and len(args) > 0:
+            batch_size = args[1]
+
+        res = self.old_fn(*args, **kwds)
+        model = self.trainer.model
+        dq_hook = SetFitModelHook(model)
+        dq_store = dq_hook.store
+        train_dataset = self.trainer.train_dataset
+        eval_dataset = self.trainer.eval_dataset
+
+        if self.column_mapping is not None:
+            train_dataset = self.trainer._apply_column_mapping(
+                train_dataset, self.trainer.column_mapping
+            )
+
+        labels = dq.get_data_logger().logger_config.labels
+        dq.init("text_classification", project_name="setfit", run_name="test")
+        if not labels:
+            labels = train_dataset.features["label"].names
+        dq.set_labels_for_run(labels)
+        datasets = [train_dataset]
+        if eval_dataset is not None:
+            if self.column_mapping is not None:
+                eval_dataset = self.trainer._apply_column_mapping(
+                    eval_dataset, self.trainer.column_mapping
+                )
+            datasets.append(eval_dataset)
+        for split in [Split.training, Split.validation]:
+            if split == Split.training:
+                dataset = train_dataset
+            else:
+                dataset = eval_dataset
+            if dataset is None:
+                continue
+            if "id" not in dataset.features:
+                dataset = dataset.map(lambda x, idx: {"id": idx}, with_indices=True)
+            for i in range(0, len(dataset), batch_size):
+                batch = dataset[i : i + batch_size]
+                model.predict_proba(batch["text"])
+                # 🔭🌕 Galileo logging
+                dq.log_data_samples(
+                    texts=batch["text"],
+                    ids=batch["id"],
+                    labels=[labels[label_id] for label_id in batch["label"]],
+                    split=split,
+                )
+                # 🔭🌕 Galileo logging
+                dq.log_model_outputs(
+                    ids=batch["id"],
+                    logits=dq_store["output"],
+                    embs=dq_store["input_args"][0],
+                    split=split,
+                    epoch=0,
+                )
+
+        return res
+
+    def unpatch(self) -> None:
+        setattr(self.trainer, self.old_fn)
+
+
+def watch(setfit: Union[SetFitModel, SetFitTrainer]) -> Optional[Callable]:
+    """Watch SetFit model by replacing predict_proba function with SetFitModelHook.
+    :param model: SetFit model"""
+    model = setfit
+
+    if isinstance(setfit, SetFitTrainer):
+        model = setfit.model
+        _PatchSetFitTrainer(setfit, "train")
+    else:
+        return evaluate(model)
+
+
+def evaluate(model: SetFitModel) -> Callable:
     """Watch SetFit model by replacing predict_proba function with SetFitModelHook.
     :param model: SetFit model
     :return: SetFitModelHook object"""
