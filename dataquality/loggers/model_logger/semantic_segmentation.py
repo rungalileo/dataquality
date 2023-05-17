785eee46d19a0a568d21c24377b7b832a5825e55
Elliott <echartock3@gmail.com>
2023-05-17 11:06:36 -0400

    Set polygon idx in lock

--- dataquality/loggers/model_logger/semantic_segmentation.py
+++ dataquality/loggers/model_logger/semantic_segmentation.py
@@ -25,6 +25,7 @@ from dataquality.utils.semantic_segmentation.polygons import (
     find_polygons_batch,
     upload_polygon_contours,
 )
+from dataquality.utils.thread_pool import lock
 
 
 class SemanticSegmentationModelLogger(BaseGalileoModelLogger):
@@ -137,11 +138,13 @@ class SemanticSegmentationModelLogger(BaseGalileoModelLogger):
                 golds.append(-1)
                 data_error_potentials.append(polygon.data_error_potential)
                 errors.append(polygon.error_type.value)
+                with lock:
+                    polygon_idx = self.logger_config.polygon_idx
+                    self.logger_config.polygon_idx = polygon_idx + 1
                 upload_polygon_contours(
-                    polygon, self.logger_config.polygon_idx, self.contours_path
+                    polygon, polygon_idx, self.contours_path
                 )
-                polygon_ids.append(self.logger_config.polygon_idx)
-                self.logger_config.polygon_idx += 1
+                polygon_ids.append(polygon_idx)
             gold_polygons = gold_polygons_batch[i]
             for polygon in gold_polygons:
                 image_ids.append(image_id)
@@ -149,11 +152,13 @@ class SemanticSegmentationModelLogger(BaseGalileoModelLogger):
                 golds.append(polygon.label_idx)
                 data_error_potentials.append(polygon.data_error_potential)
                 errors.append(polygon.error_type.value)
+                with lock:
+                    polygon_idx = self.logger_config.polygon_idx
+                    self.logger_config.polygon_idx = polygon_idx + 1
                 upload_polygon_contours(
-                    polygon, self.logger_config.polygon_idx, self.contours_path
+                    polygon, polygon_idx, self.contours_path
                 )
-                polygon_ids.append(self.logger_config.polygon_idx)
-                self.logger_config.polygon_idx += 1
+                polygon_ids.append(polygon_idx)
 
         polygon_data = {
             "id": polygon_ids,
