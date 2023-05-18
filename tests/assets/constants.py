c4efab28f84f884f0937d01392633b0688ff50f1
Bogdan Gheorghe <bogdan@rungalileo.io>
2023-05-18 12:21:50 -0400

    move test images in different dir

--- tests/assets/constants.py
+++ tests/assets/constants.py
@@ -3,4 +3,4 @@ import os
 NUMPY_SEED = 42
 
 TEST_ASSETS_DIR = f"{os.getcwd()}/tests/assets"
-TEST_IMAGES_FOLDER_ROOT = f"{TEST_ASSETS_DIR}/images/train"
+TEST_IMAGES_FOLDER_ROOT = f"{TEST_ASSETS_DIR}/train_images"
