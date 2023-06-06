b968224d87133a11a4c1459a32ff2977bfd1c800
franz101 <franz@rungalileo.io>
2023-06-06 18:09:41 -0400

    Fix SetFit notebook issues v1 (#650)
    Identified issues:
    Issues when running the dq_evaluate due to the automatic removal of the
    patches
    
    Further investigation:
    Essentiallty the cleanup manager removed the patch. The other issue was
    that it was checking for the config before it was initialized.

--- tests/integrations/setfit/test_setfit.py
+++ tests/integrations/setfit/test_setfit.py
@@ -165,3 +165,98 @@ def test_log_dataset(
     train_data = vaex.open(f"{TEST_PATH}/training/0/data/data.hdf5")
     assert train_data["meta_col"].unique() == ["meta"]
     dq.finish()
+
+
+@patch.object(ApiClient, "valid_current_user", return_value=True)
+@patch.object(dq.core.finish, "_version_check")
+@patch.object(dq.core.finish, "_reset_run")
+@patch.object(dq.core.finish, "upload_dq_log_file")
+@patch.object(ApiClient, "make_request")
+@patch.object(dq.core.finish, "wait_for_run")
+@patch.object(ApiClient, "get_project_by_name")
+@patch.object(ApiClient, "create_project")
+@patch.object(ApiClient, "get_project_run_by_name", return_value={})
+@patch.object(ApiClient, "create_run")
+@patch("dataquality.core.init._check_dq_version")
+@patch.object(
+    dq.clients.api.ApiClient,
+    "get_healthcheck_dq",
+    return_value={
+        "bucket_names": {
+            "images": "galileo-images",
+            "results": "galileo-project-runs-results",
+            "root": "galileo-project-runs",
+        },
+        "minio_fqdn": "127.0.0.1:9000",
+    },
+)
+@patch.object(dq.core.init.ApiClient, "valid_current_user", return_value=True)
+def test_end_to_end(
+    mock_valid_user: MagicMock,
+    mock_dq_healthcheck: MagicMock,
+    mock_check_dq_version: MagicMock,
+    mock_create_run: MagicMock,
+    mock_get_project_run_by_name: MagicMock,
+    mock_create_project: MagicMock,
+    mock_get_project_by_name: MagicMock,
+    set_test_config: Callable,
+    mock_wait_for_run: MagicMock,
+    mock_make_request: MagicMock,
+    mock_upload_log_file: MagicMock,
+    mock_reset_run: MagicMock,
+    mock_version_check: MagicMock,
+    cleanup_after_use: Generator,
+) -> None:
+    global dataset
+    mock_get_project_by_name.return_value = {"id": DEFAULT_PROJECT_ID}
+    mock_create_run.return_value = {"id": DEFAULT_RUN_ID}
+    set_test_config(current_project_id=None, current_run_id=None)
+
+    # 🔭🌕 Galileo logging
+    from sentence_transformers.losses import CosineSimilarityLoss
+    from setfit import SetFitModel, SetFitTrainer
+
+    import dataquality as dq
+    from dataquality.integrations.setfit import watch
+
+    model_id = "sentence-transformers/paraphrase-mpnet-base-v2"
+    model = SetFitModel.from_pretrained(model_id, use_differentiable_head=True)
+    column_mapping = {"text": "text", "label": "label"}
+    trainer = SetFitTrainer(
+        model=model,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        loss_class=CosineSimilarityLoss,
+        num_iterations=1,
+        column_mapping=column_mapping,
+    )
+    labels = ["nocat", "cat"]
+
+    # 🔭🌕 Galileo logging
+    watch(
+        trainer,
+        labels=labels,
+        # project_name=project_name, run_name=run_name,
+        batch_size=512,  # Speed up prediction
+        # 🔭🌕 Set finish to False to add test
+        finish=False,
+    )
+    trainer.freeze()
+    trainer.train()
+    trainer.unfreeze(keep_body_frozen=True)
+    dq.set_epoch(1)
+    trainer.train()
+
+    # model.save_pretrained("./trained_model")
+
+    # trainer.evaluate()
+    # unwatch(trainer)
+    dq_evaluate = watch(model, batch_size=512)
+    dq_evaluate(
+        dataset,
+        split="test",
+        column_mapping=column_mapping
+        # for inference set the split to inference
+        # and pass an inference_name="inference_run_1"
+    )
+    dq.finish()
