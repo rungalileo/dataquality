e8c974cf1dedde783984ddec06d314aa8efcc2e1
dcaustin33 <dcaustin33@gmail.com>
2023-06-07 12:16:31 -0700

    DEP runtime

--- dataquality/utils/semantic_segmentation/metrics.py
+++ dataquality/utils/semantic_segmentation/metrics.py
@@ -28,16 +28,8 @@ def calculate_and_upload_dep(
     Returns the image DEP for each image in the batch. As well as the dep_heatmaps.
         Image dep is calculated by the average pixel dep.
     """
-    import time
-    now = time.time()
     dep_heatmaps = calculate_dep_heatmaps(probs, gold_masks)
-    print(f"calculate_dep_heatmaps took {time.time() - now} seconds")
-    now = time.time()
     write_dep_to_disk(dep_heatmaps, image_ids, obj_prefix)
-    print(f"write_dep_to_disk took {time.time() - now} seconds")
-    now = time.time()
-    calculate_image_dep(dep_heatmaps)
-    print(f"calculate_image_dep took {time.time() - now} seconds")
     return calculate_image_dep(dep_heatmaps), dep_heatmaps
 
 
@@ -86,21 +78,27 @@ def calculate_dep_heatmaps(
     probs = probs.view(bs, -1, n_classes)  # (bs, n_pixels, n_classes)
     mask_size = gold_masks.shape
     gold_masks = gold_masks.view(bs, -1, 1)  # (bs, n_pixels, 1)
-
+    import time
+    now = time.time()
     gold_indices = (
         gold_masks.reshape((bs, -1, 1)).expand(-1, -1, probs.shape[2]).type(torch.int64)
     )  # (bs, n_pixels, n_classes)
     value_at_gold = torch.gather(probs, 2, gold_indices)[:, :, 0]  # (bs, n_pixels)
+    print(f"torch.gather took {time.time() - now} seconds")
+    now = time.time()
 
     next_highest = probs.clone()
     # Takes GT indices and puts 0 at that index so we don't use it as next highest value
     next_highest.scatter_(2, gold_indices, 0)
     next_highest = next_highest.max(dim=2).values
-
+    print(f"next_highest took {time.time() - now} seconds")
+    now = time.time()
     margin = value_at_gold - next_highest
     # Since margin is between -1 and 1, we normalize it to be between 0 and 1
     normalized_margin = (1 + margin) / 2
     dep_masks = 1 - normalized_margin
+    print(f"dep_masks took {time.time() - now} seconds")
+    now = time.time()
     dep_masks = dep_masks.view(mask_size)
     return dep_masks
 
