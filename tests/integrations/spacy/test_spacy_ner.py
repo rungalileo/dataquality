f655cfc79515a5d90be35bf4ac0fd554aa528b49
Anthony Corletti <anthony@rungalileo.io>
2023-06-07 11:53:34 -0400

    cleanup

--- tests/integrations/spacy/test_spacy_ner.py
+++ tests/integrations/spacy/test_spacy_ner.py
@@ -455,14 +455,18 @@ def test_spacy_inference_only(
     # Since order might change due to multi-threading we verify each embedding
     # is in the expected list, but don't check the exact order
     embs_expected = TestSpacyInfExpectedResults.gt_embs
-    # Check if all elements in array1 have a close value in array2
+    # When running with pytest-xdist, the embeddings are slightly different
+    # due to the fact that test runs are parallelized and resources are shared
+    # between them. We allow for larger absolute and relative tolerances in this case.
     if os.getenv("PYTEST_XDIST_WORKER_COUNT"):
-        np.allclose(embs, embs_expected, atol=1e-3)
+        is_close = np.isclose(embs[:, np.newaxis], embs_expected, atol=1e-3, rtol=3)
     else:
         is_close = np.isclose(embs[:, np.newaxis], embs_expected, atol=1e-3)
-        # For each element in array1, check if any element in array2 is close
-        any_close = np.any(is_close, axis=1)
-        assert np.all(any_close)
+    # For each element in array1, check if any element in array2 is close
+    any_close = np.any(is_close, axis=1)
+
+    # Check if all elements in array1 have a close value in array2
+    assert np.all(any_close)
     # arrange the probs array to account for misordering of logged samples
     assert len(probs) == 7
     probs = probs.sort(["sample_id", "span_start"])
