41870088316a960a2a7b7d4a3f464ff5aec98071
Franz Gusto <franz@rungalileo.io>
2023-06-14 00:43:58 -0400

    fix formatting

--- dataquality/loggers/data_logger/text_classification.py
+++ dataquality/loggers/data_logger/text_classification.py
@@ -299,9 +299,8 @@ class TextClassificationDataLogger(BaseGalileoDataLogger):
         )
 
         # Make sure the class labels are set
-        if (
-            self.logger_config.labels
-            and not isinstance(dataset.features[label], ClassLabel)
+        if self.logger_config.labels and not isinstance(
+            dataset.features[label], ClassLabel
         ):
             dataset = add_class_label_to_dataset(dataset, self.logger_config.labels)
 
