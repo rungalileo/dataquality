rungalileo/dataquality

commit 45415c56dccfe0c63552583d39dd42dd0d55d22d

chore: Add py37 warning (#720)
During the import of `dataquality`, start warning users that we're going
to be dropping Python 3.7 support soon.

dataquality/__init__.py
@@ -35,9 +35,11 @@ __version__ = "0.9.8"
 
 import sys
 from typing import Any, List, Optional
+from warnings import warn
 
 import dataquality.core._config
 import dataquality.integrations
+from dataquality.exceptions import GalileoWarning
 
 # We try/catch this in case the user installed dq inside of jupyter. You need to
 # restart the kernel after the install and we want to make that clear. This is because
@@ -142,6 +144,16 @@ try:
     resource.setrlimit(resource.RLIMIT_NOFILE, (65535, 65535))
 except (ImportError, ValueError):  # The users limit is higher than our max, which is OK
     pass
+
+# Warn if the user is using an old version of Python.
+if sys.version_info < (3, 8):
+    warn(
+        "You are using an old version of Python. Please upgrade to Python 3.8"
+        "or higher. dataquality will stop supporting Python 3.7 in the near "
+        "future.",
+        GalileoWarning,
+    )
+
 
 #  Logging is optional. If enabled, imports, method calls
 #  and exceptions can be logged by calling the logger.

dataquality/integrations/ultralytics.py
@@ -3,9 +3,8 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from urllib.parse import urljoin
 
 import torch
-import ultralytics
 from torchvision.ops.boxes import box_convert
-from ultralytics import YOLO
+from ultralytics import YOLO, checks
 from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.engine.trainer import BaseTrainer
 from ultralytics.yolo.engine.validator import BaseValidator
@@ -24,7 +23,7 @@ from dataquality.schemas.task_type import TaskType
 from dataquality.utils.dqyolo import CONF_DEFAULT, IOU_DEFAULT
 from dataquality.utils.ultralytics import non_max_suppression, process_batch_data
 
-ultralytics.checks()
+checks()
 
 Coordinates = Union[Tuple, List]
 

commit 4872bc074ac9f81999a6fbea577b5ee51e9ee87d

fix: Use tmpdir for Ultralytics' YOLO config (#722)
Turns out the test that was failing in #720 was just being flaky,
because of an upstream change in settings management for `ultralytics`
(https://github.com/ultralytics/ultralytics/pull/3790).

This improves how we handle it.

Tests:
- [x] Local.
- [ ] CI.

tests/integrations/ultralytics/conftest.py
@@ -1,0 +1,15 @@
+from typing import Generator
+
+from pytest import MonkeyPatch, fixture
+
+
+@fixture(autouse=True, scope="function")
+def set_ultralytics_config_dir(
+    tmpdir: str, monkeypatch: MonkeyPatch
+) -> Generator[None, None, None]:
+    # Ensure we use different config dirs for each time `ultralytics` is imported.
+    # https://github.com/ultralytics/ultralytics/blob/main/ultralytics/utils/__init__.py
+    # #L550-L582
+    monkeypatch.setenv("YOLO_CONFIG_DIR", tmpdir)
+    yield
+    monkeypatch.delenv("YOLO_CONFIG_DIR")