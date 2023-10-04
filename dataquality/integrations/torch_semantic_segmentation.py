53ce2861c275e5c53befda8b4220e538bdee5146
dcaustin33 <dcaustin33@gmail.com>
2023-10-04 15:52:04 -0400

    LM only in dq finish

--- dataquality/integrations/torch_semantic_segmentation.py
+++ dataquality/integrations/torch_semantic_segmentation.py
@@ -446,10 +446,10 @@ class SemanticTorchLogger(TorchLogger):
             if gold_mask.dtype == torch.float16:
                 gold_mask = gold_mask.to(torch.float32)
 
-            mislabeled_pixels = self.calculate_mislabeled_pixels(probs, gold_mask)
-            # do not log if we are not in the final inference loop
             if not self.called_finish:
                 return
+            mislabeled_pixels = self.calculate_mislabeled_pixels(probs, gold_mask)
+            # do not log if we are not in the final inference loop
             logger = SemanticSegmentationModelLogger(
                 imgs_remote_location=self.imgs_remote_location,
                 image_paths=image_paths,
