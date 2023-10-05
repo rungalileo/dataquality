48e592a5101263d9aa97c122fe31511bbb6cbf33
franz <franz@rungalileo.io>
2023-10-05 03:10:25 +0200

    added test

--- dataquality/integrations/lightning.py
+++ dataquality/integrations/lightning.py
@@ -1,37 +1,29 @@
 # Imports for the hook manager
-from typing import Any, Callable, Optional, Union
-from transformers.modeling_outputs import TokenClassifierOutput
-
-from torch import Tensor
-from torch.nn import Module
-
-from dataquality.exceptions import GalileoException
-from dataquality.utils.helpers import map_indices_to_ids
-from dataquality.utils.patcher import PatchManager
+from typing import Callable, Optional
 
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks import Callback
-from lightning.pytorch.utilities.types import STEP_OUTPUT
 
 import dataquality as dq
+from dataquality.integrations.torch import TorchLogger
 from dataquality.schemas.split import Split
-from dataquality.schemas.torch import InputDim, Layer
+from dataquality.schemas.torch import DimensionSlice, Layer
+from dataquality.utils.patcher import PatchManager
 from dataquality.utils.torch import (
     ModelHookManager,
     PatchDataloadersGlobally,
-    TorchBaseInstance,
     TorchHelper,
 )
 
 
-class DQCallback(Callback, TorchBaseInstance, PatchManager):
+class DQCallback(Callback, TorchLogger, PatchManager):
     hook_manager: ModelHookManager
 
     def __init__(
         self,
         last_hidden_state_layer: Optional[Layer] = None,
-        embedding_dim: Optional[InputDim] = None,
-        logits_dim: Optional[InputDim] = None,
+        embedding_dim: Optional[DimensionSlice] = None,
+        logits_dim: Optional[DimensionSlice] = None,
         classifier_layer: Layer = "classifier",
         embedding_fn: Optional[Callable] = None,
         logits_fn: Optional[Callable] = None,
@@ -51,91 +43,17 @@ class DQCallback(Callback, TorchBaseInstance, PatchManager):
         helper_data = dq.get_model_logger().logger_config.helper_data
         self.torch_helper_data = TorchHelper()
         helper_data["torch_helper"] = self.torch_helper_data
-        PatchDataloadersGlobally(self.torch_helper_data)
         self._init_dimension(self.embedding_dim, self.logits_dim)
-        self.model = trainer.model
+        self.model = pl_module.model
         self._init_helper_data(self.hook_manager, self.model)
+        PatchDataloadersGlobally(self.torch_helper_data)
         self.logger_config = dq.get_data_logger().logger_config
-
-    def _init_helper_data(self, hm: ModelHookManager, model: Module) -> None:
-        """
-        Initialize the helper data with ids from the dataloader indices,
-        patches for applied monkey patched functions and the hook manager.
-        :param hm: Hook manager
-        """
-        self.torch_helper_data.clear()
-        self.torch_helper_data = TorchHelper(model, hm)
-
-    def _attach_hooks_to_model(
-        self, model: Module, classifier_layer: Layer, last_hidden_state_layer: Layer
-    ) -> None:
-        """
-        Method to attach hooks to the model by using the hook manager
-        :param model: Model
-        :param model: pytorch model layer to attach hooks to
-        """
-        try:
-            self.hook_manager.attach_classifier_hook(
-                model, self._dq_classifier_hook_with_step_end, classifier_layer
-            )
-        except Exception as e:
-            self.hook_manager.attach_hooks_to_model(
-                model, self._dq_embedding_hook, last_hidden_state_layer
-            )
-            self.hook_manager.attach_hook(model, self._dq_logit_hook_with_step_end)
-
-    def _dq_logit_hook_with_step_end(
-        self,
-        model: Module,
-        model_input: Tensor,
-        model_output: Union[TokenClassifierOutput, Tensor],
-    ) -> None:
-        """
-        Hook to extract the logits from the model.
-        :param model: Model pytorch model
-        :param model_input: Model input
-        :param model_output: Model output
-        """
-        self._dq_logit_hook(model, model_input, model_output)
-        self._on_step_end()
-
-    def _on_step_end(self) -> None:
-        """Log the embeddings, ids and logits.
-
-        We save the embeddings and logits in a dict called model_output_store
-        in the helper data. This is because the embeddings and logits are
-        extracted in the hooks and we need to log them in the on_step_end
-        method.
-        """
-
-        model_outputs_store = self.torch_helper_data.model_outputs_store
-        # Workaround for multiprocessing
-        if model_outputs_store.ids is None and len(
-            self.torch_helper_data.dl_next_idx_ids
-        ):
-            model_outputs_store.ids = self.torch_helper_data.dl_next_idx_ids.pop(0)
-
-        # Log only if embedding exists
-        assert model_outputs_store.embs is not None, GalileoException(
-            "Embedding passed to the logger can not be logged"
+        self._attach_hooks_to_model(
+            model=self.model,
+            classifier_layer=self.classifier_layer,
+            last_hidden_state_layer=self.last_hidden_state_layer,
         )
-        assert model_outputs_store.logits is not None, GalileoException(
-            "Logits passed to the logger can not be logged"
-        )
-        assert model_outputs_store.ids is not None, GalileoException(
-            "id column missing in dataset (needed to map rows to the indices/ids)"
-        )
-        # Convert the indices to ids
-        cur_split = self.logger_config.cur_split
-        assert cur_split is not None, GalileoException(
-            "Current split must be set before logging"
-        )
-        cur_split = cur_split.lower()  # type: ignore
-        model_outputs_store.ids = map_indices_to_ids(
-            self.logger_config.idx_to_id_map[cur_split], model_outputs_store.ids
-        )
-        dq.log_model_outputs(**model_outputs_store.to_dict())
-        model_outputs_store.clear()
+        dq.set_epoch(0)
 
     def on_fit_end(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
@@ -148,72 +66,18 @@ class DQCallback(Callback, TorchBaseInstance, PatchManager):
     ) -> None:
         """Called when the train epoch begins."""
         dq.set_split(Split.training)
+        dq.set_epoch(trainer.current_epoch)
 
     def on_validation_epoch_start(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
         """Called when the val epoch begins."""
         dq.set_split(Split.validation)
+        dq.set_epoch(trainer.current_epoch)
 
     def on_test_epoch_start(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
         """Called when the test epoch begins."""
         dq.set_split(Split.test)
-
-    def on_test_batch_end(
-        self,
-        trainer: "pl.Trainer",
-        pl_module: "pl.LightningModule",
-        outputs: STEP_OUTPUT,
-        batch: Any,
-        batch_idx: int,
-    ) -> None:
-        pass
-
-    def on_test_batch_end(
-        self,
-        trainer: "pl.Trainer",
-        pl_module: "pl.LightningModule",
-        outputs: STEP_OUTPUT,
-        batch: Any,
-        batch_idx: int,
-    ) -> None:
-        pass
-
-    def on_validation_batch_end(
-        self,
-        trainer: "pl.Trainer",
-        pl_module: "pl.LightningModule",
-        outputs: STEP_OUTPUT,
-        batch: Any,
-        batch_idx: int,
-    ) -> None:
-        pass
-
-    def on_train_batch_start(
-        self,
-        trainer: "pl.Trainer",
-        pl_module: "pl.LightningModule",
-        batch: Any,
-        batch_idx: int,
-    ) -> None:
-        """Called when the train batch begins."""
-        pass
-
-    def on_train_batch_end(
-        self,
-        trainer: "pl.Trainer",
-        pl_module: "pl.LightningModule",
-        outputs: STEP_OUTPUT,
-        batch: Any,
-        batch_idx: int,
-    ) -> None:
-        """Called when the train batch ends.
-
-        Note:
-            The value ``outputs["loss"]`` here will be the normalized value w.r.t ``accumulate_grad_batches`` of the
-            loss returned from ``training_step``.
-
-        """
-        pass
+        dq.set_epoch(trainer.current_epoch)
--- tests/integrations/lightning/test_lightning.py
+++ tests/integrations/lightning/test_lightning.py
@@ -1,100 +1,68 @@
-from typing import Any, Callable, Generator
+from typing import Callable, Generator
 from unittest.mock import MagicMock, patch
 
-import evaluate
-import numpy as np
-import torch
+import lightning.pytorch as pl
 import vaex
-from torch.nn import Module
-from torch.utils.data import DataLoader
-from transformers import (
-    AutoModel,
-    AutoModelForSequenceClassification,
-    Trainer,
-    TrainingArguments,
-)
-import pytorch_lightning as pl
+from torch import nn
+from torch.nn import functional as F
 from torch.optim import AdamW
+from torch.utils.data import DataLoader
 
+# %%
+from torchvision import datasets, transforms
 
 import dataquality as dq
-from dataquality import config
-from dataquality.integrations.transformers_trainer import watch
-from dataquality.schemas.split import Split
+from dataquality.integrations.lightning import DQCallback
 from dataquality.schemas.task_type import TaskType
 from dataquality.utils.thread_pool import ThreadPoolManager
-from tests.conftest import HF_TEST_BERT_PATH, TestSessionVariables, model, tokenizer
-from tests.test_utils.hf_datasets_mock import mock_hf_dataset, mock_hf_dataset_repeat
-from tests.test_utils.mock_request import (
-    mocked_create_project_run,
-    mocked_get_project_run,
-)
-from tests.test_utils.pt_datasets_mock import CustomDatasetWithTokenizer
-
-metric = evaluate.load("accuracy")
-
-
-def preprocess_function(examples, tokenizer):
-    return tokenizer(
-        examples["text"],
-        padding="max_length",
-        max_length=201,
-        truncation=True,
-    )
-
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = predictions.argmax(axis=1)
-    return metric.compute(predictions=predictions, references=labels)
-
-
-# 🔭🌕 Galileo logging
-mock_dataset_with_ids = mock_hf_dataset.map(
-    lambda x, idx: {"id": idx}, with_indices=True
-)
-mock_dataset_with_ids_repeat = mock_hf_dataset_repeat.map(
-    lambda x, idx: {"id": idx}, with_indices=True
+from tests.conftest import (
+    TestSessionVariables,
 )
 
-encoded_train_dataset = mock_dataset_with_ids.map(
-    lambda x: preprocess_function(x, tokenizer), batched=True
-)
-encoded_test_dataset = mock_dataset_with_ids.map(
-    lambda x: preprocess_function(x, tokenizer), batched=True
-)
-
-encoded_train_dataset_repeat = mock_dataset_with_ids_repeat.map(
-    lambda x: preprocess_function(x, tokenizer), batched=True
-)
-encoded_test_dataset_repeat = mock_dataset_with_ids_repeat.map(
-    lambda x: preprocess_function(x, tokenizer), batched=True
+load_transforms = transforms.Compose(
+    [
+        transforms.Resize(70),
+        transforms.CenterCrop(64),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
 )
 
 
 # Define the PyTorch Lightning model
 class LightningModel(pl.LightningModule):
-    def __init__(self, model, tokenizer):
-        super(LightningModel, self).__init__()
-        self.model = model
-        self.tokenizer = tokenizer
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(3 * 64 * 64, 256),
+            nn.ReLU(),
+            nn.Linear(256, 2),  # Output two values for binary classification
+        )
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
 
     def training_step(self, batch, batch_idx):
-        outputs = self.model(**batch)
-        loss = outputs.loss
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        logits = self.model(x)
+        loss = F.cross_entropy(logits, y)  # Use cross-entropy loss for classification
+        self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        outputs = self.model(**batch)
-        predictions = outputs.logits.argmax(axis=1)
-        labels = batch["labels"]
-        acc = (predictions == labels).float().mean()
-        self.log(
-            "val_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        logits = self.model(x)
+        loss = F.cross_entropy(logits, y)
+        self.log("val_loss", loss)
+        return loss
 
     def configure_optimizers(self):
-        return AdamW(self.model.parameters(), lr=3e-4)
+        return AdamW(self.parameters(), lr=1e-3)
+
+
+model = LightningModel()
 
 
 @patch.object(dq.core.init.ApiClient, "valid_current_user", return_value=True)
@@ -102,7 +70,7 @@ class LightningModel(pl.LightningModule):
 @patch.object(dq.core.finish, "upload_dq_log_file")
 @patch.object(dq.clients.api.ApiClient, "make_request")
 @patch.object(dq.core.finish, "wait_for_run")
-def test_hf_watch_with_pt_dataset_e2e(
+def test_lightning_cv_end_to_end(
     mock_wait_for_run: MagicMock,
     mock_make_request: MagicMock,
     mock_upload_log_file: MagicMock,
@@ -113,35 +81,40 @@ def test_hf_watch_with_pt_dataset_e2e(
     test_session_vars: TestSessionVariables,
 ) -> None:
     """Base case: Tests creating a new project and run"""
-    set_test_config(task_type=TaskType.text_classification)
-    # 🔭🌕 Galileo logging
-    dq.set_labels_for_run(["pos", "neg"])
-    train_dataset = CustomDatasetWithTokenizer(tokenizer)
-    val_dataset = CustomDatasetWithTokenizer(tokenizer)
-    test_dataset = CustomDatasetWithTokenizer(tokenizer)
-    dq.log_dataset(mock_dataset_with_ids, split="train")
-    dq.log_dataset(mock_dataset_with_ids, split="validation")
-    dq.log_dataset(mock_dataset_with_ids, split="test")
 
-    lightning_model = LightningModel(model_seq, tokenizer)
-    trainer = pl.Trainer(max_epochs=1)
-    trainer.fit(lightning_model, train_dataloader, val_dataloader)
+    train_dataset = datasets.ImageFolder(
+        "tests/assets/train_images", transform=load_transforms
+    )
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=4, shuffle=True, num_workers=0
+    )
+    # train the model (hint: here are some helpful
+    # Trainer arguments for rapid idea iteration)
 
+    set_test_config(task_type=TaskType.image_classification)
     # 🔭🌕 Galileo logging
-    watch(trainer)
-    trainer.train()
-    trainer.predict(val_dataset)
+    dq.set_labels_for_run(["labelA", "labelB"])
+
+    dq.log_image_dataset(train_dataset, split="train")
+    dq.log_image_dataset(train_dataset, split="validation")
+    dq.log_image_dataset(train_dataset, split="test")
+    callback = DQCallback(classifier_layer=model.model[2])
+    trainer = pl.Trainer(limit_train_batches=100, max_epochs=1, callbacks=[callback])
+    trainer.fit(
+        model=model,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=train_dataloader,
+    )
+
     ThreadPoolManager.wait_for_threads()
+    dq.get_data_logger().upload()
     # All data for splits should be logged
     assert len(vaex.open(f"{test_session_vars.LOCATION}/training/0/*.hdf5")) == len(
         train_dataset
     )
     assert len(vaex.open(f"{test_session_vars.LOCATION}/validation/0/*.hdf5")) == len(
-        val_dataset
-    )
-    assert len(vaex.open(f"{test_session_vars.LOCATION}/test/0/*.hdf5")) == len(
-        test_dataset
+        train_dataset
     )
 
-    # Should upload without failing on data validation or otherwise
+    vaex.open(f"{test_session_vars.TEST_PATH}/training/0/data/data.*")
     dq.finish()
