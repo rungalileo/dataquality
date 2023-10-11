1568ce178ad9421b3a9c017b172b6d5080ac9105
Elliott <echartock3@gmail.com>
2023-10-11 13:21:24 -0400

    fix: refactor auto to config files

--- dataquality/integrations/seq2seq/auto.py
+++ dataquality/integrations/seq2seq/auto.py
@@ -1,12 +1,16 @@
-from typing import List, Optional, Union
+from typing import List, Optional
 
-import pandas as pd
 from datasets import Dataset, DatasetDict
 from transformers import Trainer
 
 import dataquality as dq
 from dataquality.dq_auto.base_data_manager import BaseDatasetManager
 from dataquality.integrations.seq2seq.s2s_trainer import do_train, get_trainer
+from dataquality.integrations.seq2seq.schema import (
+    AutoDatasetConfig,
+    AutoGenerationConfig,
+    AutoTrainingConfig,
+)
 from dataquality.schemas.split import Split
 from dataquality.schemas.task_type import TaskType
 from dataquality.utils.auto import (
@@ -61,19 +65,12 @@ def _log_dataset_dict(dd: DatasetDict, input_col: str, target_col: str) -> None:
 
 
 def auto(
-    hf_data: Optional[Union[DatasetDict, str]] = None,
-    train_data: Optional[Union[pd.DataFrame, Dataset, str]] = None,
-    val_data: Optional[Union[pd.DataFrame, Dataset, str]] = None,
-    test_data: Optional[Union[pd.DataFrame, Dataset, str]] = None,
-    num_train_epochs: int = 3,
-    hf_model: str = "google/flan-t5-base",
     project_name: str = "auto_s2s",
     run_name: Optional[str] = None,
+    dataset_config: Optional[AutoDatasetConfig] = None,
+    training_config: Optional[AutoTrainingConfig] = None,
+    generation_config: Optional[AutoGenerationConfig] = None,
     wait: bool = True,
-    max_input_tokens: Optional[int] = None,
-    max_target_tokens: Optional[int] = None,
-    create_data_embs: Optional[bool] = None,
-    generation_splits: Optional[List[str]] = None,
 ) -> Trainer:
     """Automatically gets insights on a Seq2Seq dataset
 
@@ -84,47 +81,25 @@ def auto(
     One of `hf_data`, `train_data` should be provided. If neither of those are, a
     demo dataset will be loaded by Galileo for training.
 
-    :param hf_data: Union[DatasetDict, str] Use this param if you have huggingface
-        data in the hub or in memory. Otherwise see `train_data`, `val_data`,
-        and `test_data`. If provided, train_data, val_data, and test_data are ignored
-    :param train_data: Optional training data to use. Can be one of
-        * Pandas dataframe
-        * Huggingface dataset
-        * Path to a local file
-        * Huggingface dataset hub path
-    :param val_data: Optional validation data to use. The validation data is what is
-        used for the evaluation dataset in huggingface, and what is used for early
-        stopping. If not provided, but test_data is, that will be used as the evaluation
-        set. If neither val nor test are available, the train data will be randomly
-        split 80/20 for use as evaluation data.
-        Can be one of
-        * Pandas dataframe
-        * Huggingface dataset
-        * Path to a local file
-        * Huggingface dataset hub path
-    :param test_data: Optional test data to use. The test data, if provided with val,
+    The validation data is what is
+    used for the evaluation dataset in huggingface, and what is used for early
+    stopping. If not provided, but test_data is, that will be used as the evaluation
+    set. If neither val nor test are available, the train data will be randomly
+    split 80/20 for use as evaluation data.
+
+    The test data, if provided with val,
         will be used after training is complete, as the held-out set. If no validation
         data is provided, this will instead be used as the evaluation set.
-        Can be one of
-        * Pandas dataframe
-        * Huggingface dataset
-        * Path to a local file
-        * Huggingface dataset hub path
-    :param num_train_epochs: Optional num training epochs. If not set, we default to 3
-    :param hf_model: The pretrained AutoModel from huggingface that will be used to
-        tokenize and train on the provided data. Default google/flan-t5-base
+
+
+
+
     :param project_name: Optional project name. If not set, a random name will
         be generated
     :param run_name: Optional run name for this data. If not set, a random name will
         be generated
     :param wait: Whether to wait for Galileo to complete processing your run.
         Default True
-    :param max_input_tokens: Optional max input tokens. If not set, we default to 512
-    :param max_target_tokens: Optional max target tokens. If not set, we default to 128
-    :param create_data_embs: Whether to create data embeddings for this run. Default
-        False
-    :param generation_splits: Optional list of splits to generate on. If not set, we
-        default to ["test"]
 
     To see auto insights on a random, pre-selected dataset, simply run
     ```python
@@ -157,16 +132,20 @@ def auto(
     )
     ```
     """
+    dataset_config = dataset_config or AutoDatasetConfig()
+    training_config = training_config or AutoTrainingConfig()
+    generation_config = generation_config or AutoGenerationConfig()
+
     manager = S2SDatasetManager()
     dd = manager.get_dataset_dict(
-        hf_data,
-        train_data=train_data,
-        val_data=val_data,
-        test_data=test_data,
+        dataset_config.hf_data,
+        train_data=dataset_config.train_data,
+        val_data=dataset_config.val_data,
+        test_data=dataset_config.test_data,
     )
     dq.login()
-    if not run_name and isinstance(hf_data, str):
-        run_name = run_name_from_hf_dataset(hf_data)
+    if not run_name and isinstance(dataset_config.hf_data, str):
+        run_name = run_name_from_hf_dataset(dataset_config.hf_data)
 
     dq.init(TaskType.seq2seq, project_name=project_name, run_name=run_name)
     input_col = manager.formatter.input_col
@@ -175,13 +154,11 @@ def auto(
     # We 'watch' in get_trainer, which must happen before logging datasets
     model, dataloaders = get_trainer(
         dd,
-        hf_model,
-        input_col,
-        target_col,
-        max_input_tokens,
-        max_target_tokens,
-        generation_splits,
+        dataset_config.input_col,
+        dataset_config.target_col,
+        training_config,
+        generation_config,
     )
 
     _log_dataset_dict(dd, input_col=input_col, target_col=target_col)
-    return do_train(model, dataloaders, num_train_epochs, wait, create_data_embs)
+    return do_train(model, dataloaders, training_config, wait)
--- dataquality/integrations/seq2seq/schema.py
+++ dataquality/integrations/seq2seq/schema.py
@@ -1,23 +1,94 @@
-# TODO: create config schema for training, data, and generation
 from dataclasses import dataclass
+from typing import List, Optional, Union
+
+import pandas as pd
+from datasets import Dataset, DatasetDict
+
+
+@dataclass
+class AutoDatasetConfig:
+    """Configuration for creating a dataset from a file or object
+
+    One of `hf_name`, `train_path` or `train_dataset` should be provided. If none of
+    those is, a demo dataset will be loaded by Galileo for training.
+
+
+    :param hf_data: Union[DatasetDict, str] Use this param if you have huggingface
+        data in the hub or in memory. Otherwise see `train_data`, `val_data`,
+        and `test_data`. If provided, train_data, val_data, and test_data are ignored
+    :param train_path: Optional path to training data file to use. Can be one of
+        * Path to a local file
+        * Huggingface dataset hub path
+    :param val_path: Optional path to validation data to use. Can be one of
+        * Path to a local file
+        * Huggingface dataset hub path
+    :param test_path: Optional test data to use. Can be one of
+        * Path to a local file
+        * Huggingface dataset hub path
+    :param train_data: Optional training data to use. Can be one of
+        * Pandas dataframe
+        * Huggingface dataset
+    :param val_data: Optional validation data to use. Can be one of
+        * Pandas dataframe
+        * Huggingface dataset
+    :param test_data: Optional test data to use. Can be one of
+        * Pandas dataframe
+        * Huggingface dataset
+    :param input_col: Column name for input data, defaults to "text"
+    :param target_col: Column name for target data, defaults to "label"
+    """
+
+    hf_data: Optional[Union[DatasetDict, str]] = None
+    # If dataset provided as path to local file
+    train_path: Optional[str] = None
+    val_path: Optional[str] = None
+    test_path: Optional[str] = None
+    # If dataset provided as object
+    train_data: Optional[Union[pd.DataFrame, Dataset]] = None
+    val_data: Optional[Union[pd.DataFrame, Dataset]] = None
+    test_data: Optional[Union[pd.DataFrame, Dataset]] = None
+    # Column names
+    input_col: str = "text"
+    target_col: str = "label"
 
 
 @dataclass
 class AutoTrainingConfig:
-    name: str
-    input_col: str
-    target_col: str
+    """Configuration for training a seq2seq model
 
+    :param model: The pretrained AutoModel from huggingface that will be used to
+        tokenize and train on the provided data. Default google/flan-t5-base
+    :param num_train_epochs: Optional num training epochs. If not set, we default to 3
+    :param max_input_tokens: Optional max input tokens. If not set, we default to 512
+    :param max_target_tokens: Optional max target tokens. If not set, we default to 128
+    :param create_data_embs: Whether to create data embeddings for this run. Default
+        False
+    """
 
-@dataclass
-class AutoDataConfig:
-    name: str
-    input_col: str
-    target_col: str
+    model: str = "google/flan-t5-base"
+    epochs: int = 3
+    learning_rate = 3e-4
+    accumulation_steps = 4
+    batch_size = 4
+    create_data_embs: Optional[bool] = None
+    max_input_tokens: int = 512
+    max_target_tokens: int = 128
 
 
 @dataclass
 class AutoGenerationConfig:
-    name: str
-    input_col: str
-    target_col: str
+    """Configuration for generating insights from a trained seq2seq model
+
+    :param generation_splits: Optional list of splits to generate on. If not set, we
+        default to ["test"]
+    """
+
+    # The default values in Generation Config
+    # Check more out here: https://huggingface.co/docs/transformers/v4.30.0/en/main_classes/text_generation#transformers.GenerationConfig
+    max_new_tokens: int = 16
+    temperature: float = 0.2
+    # Whether we use multinomial sampling
+    do_sample: bool = False
+    top_p: float = 1.0
+    top_k: int = 50
+    generation_splits: Optional[List[str]] = None
