126f7bb87952175183f4f3f34e8e411842d3dd1f
Elliott <echartock3@gmail.com>
2023-11-07 10:45:26 -0800

    rename base to seq2seq base

--- dataquality/loggers/data_logger/seq2seq/completion.py
+++ dataquality/loggers/data_logger/seq2seq/completion.py
@@ -1,4 +1,4 @@
-from dataquality.loggers.data_logger.seq2seq.base import Seq2SeqDataLogger
+from dataquality.loggers.data_logger.seq2seq.seq2seq_base import Seq2SeqDataLogger
 from dataquality.loggers.logger_config.seq2seq.completion import (
     Seq2SeqCompletionLoggerConfig,
     seq2seq_completion_logger_config,
--- dataquality/loggers/model_logger/seq2seq/chat.py
+++ dataquality/loggers/model_logger/seq2seq/chat.py
@@ -2,7 +2,7 @@ from dataquality.loggers.logger_config.seq2seq.chat import (
     Seq2SeqChatLoggerConfig,
     seq2seq_chat_logger_config,
 )
-from dataquality.loggers.model_logger.seq2seq.base import Seq2SeqModelLogger
+from dataquality.loggers.model_logger.seq2seq.seq2seq_base import Seq2SeqModelLogger
 
 
 class Seq2SeqChatModelLogger(Seq2SeqModelLogger):
