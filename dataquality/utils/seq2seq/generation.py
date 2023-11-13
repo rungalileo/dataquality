0d97553e7fdfe1e84032392eb32836515047c0a9
Jonathan Gomes Selman <jonathan@rungalileo.io>
2023-11-13 16:01:20 -0500

    Starting generation work

--- dataquality/utils/seq2seq/generation.py
+++ dataquality/utils/seq2seq/generation.py
@@ -103,6 +103,87 @@ def generate_sample_output(
     )
 
 
+@torch.no_grad()
+def generate_sample_output_decoder_only(
+    input_str: str,
+    model: PreTrainedModel,
+    tokenizer: PreTrainedTokenizerFast,
+    max_input_tokens: int,
+    generation_config: GenerationConfig,
+) -> ModelGeneration:
+    """Generate and extract model logprobs
+
+    Tokenize the input string and then use `hf.generate`
+    to generate just the output tokens.
+
+    We don't rely on the scores returned by hf since these
+    can be altered by hf internally depending on the generation
+    config / can be hard to parse in the case of BEAM Search.
+
+    Instead, we pass the generated output back through the model
+    to extract the token logprobs. We effectively ask the model
+    to evaluate its own generation - which is identical to generation
+    because of causal language modeling.
+
+    Parameters:
+    -----------
+    input_str: str
+        Input string context used to seed the generation
+    tokenizer: PreTrainedTokenizerFast
+    max_input_tokens: the max number of tokens to use for tokenization
+    model: PreTrainedModel
+    generation_config: GenerationConfig
+        Users generation config specifying the parameters for generation
+
+    Return:
+    -------
+    model_generation: ModelGeneration
+        - generated_ids: np.ndarray of shape - [seq_len]
+        - generated_token_logprobs: np.ndarray of shape - [seq_len]
+        - generated_top_logprobs: List[List[Tuple[str, float]]]
+    """
+    # Shape - [1, seq_len]
+    # We make sure to tokenize with the same max_length as they did during training
+    input_ids = tokenizer(
+        input_str,
+        truncation=True,
+        max_length=max_input_tokens,
+        verbose=False,
+        return_tensors="pt",
+    )["input_ids"].to(model.device)
+
+    gen_ids = model.generate(
+        input_ids=input_ids,
+        generation_config=generation_config,
+    )
+
+    # Strip the beginning <pad> token and keep as Tensor
+    gen_ids = gen_ids[..., 1:]
+
+    # Pass the generated output through the model to get the logits
+    model_outputs = model(input_ids=input_ids, labels=gen_ids)
+
+    logits = model_outputs.logits
+    logprobs = torch.nn.functional.log_softmax(logits, dim=-1).cpu().numpy()
+
+    # Remove singleton batch dimension
+    logprobs = logprobs.squeeze(0)  # [seq_len, vocab_size]
+    gen_ids = gen_ids.squeeze(0).cpu().numpy()  # [seq_len]
+
+    top_logprobs_indices = get_top_logprob_indices(logprobs)
+
+    gen_logprob_data = process_sample_logprobs(
+        logprobs,
+        sample_labels=gen_ids,
+        sample_top_indices=top_logprobs_indices,
+        tokenizer=tokenizer,
+    )
+
+    return ModelGeneration(
+        generated_ids=gen_ids, generated_logprob_data=gen_logprob_data
+    )
+
+
 def generate_on_batch(
     texts: pa.array,
     model: PreTrainedModel,
