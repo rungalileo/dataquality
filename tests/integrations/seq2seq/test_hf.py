378b7976ddee285abe6051c85af67ad53d7550bc
Bogdan Gheorghe <112427971+bogdan-galileo@users.noreply.github.com>
2023-11-14 11:27:37 -0500

    chore: add support for Tokenizer (#797)
    Adding support for tokenizers of type Tokenizer (to support Cohere)
    https://app.shortcut.com/galileo/story/8963
    
    The class `from tokenizers import Tokenizer` is a performant
    implementation of lots of popular tokenizers
    https://github.com/huggingface/tokenizers. It is implemented in Rust
    with bindings in Python.
    
    The class `from transformers import PreTrainedTokenizerFast` seems more
    popular as it integrates easier with transformers and many trained
    tokenizers have this type
    https://huggingface.co/docs/transformers/v4.33.2/en/main_classes/tokenizer#transformers.PreTrainedTokenizerFast.
    
    We first implemented support for PreTrainedTokenizerFast and require
    support for Tokenizer (in particular for Cohere). It turns out a
    Tokenizer can be converted into a PreTrainedTokenizerFast, so that's
    what we'll use for now since it allows minimal code change. We can think
    about a more general solution if we'll need to support more general
    tokenizers

---
+++ tests/integrations/seq2seq/test_hf.py
@@ -0,0 +1,61 @@
+from typing import Callable
+
+from pytest import raises
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from transformers import PreTrainedTokenizerFast
+
+from dataquality.integrations.seq2seq.hf import set_tokenizer
+from tests.conftest import TestSessionVariables, tokenizer_T5, tokenizer_T5_not_auto
+
+
+def test_set_tokenizer_PreTrainedFastTokenizer(
+    set_test_config: Callable,
+    cleanup_after_use: Callable,
+    test_session_vars: TestSessionVariables,
+) -> None:
+    """Test that we can use a PreTrainedFastTokenizer from Transformers"""
+    set_test_config(task_type="seq2seq")
+
+    # Check that we can set the T5 auto tokenizer (of type PreTrainedTokenizerFast)
+    assert isinstance(tokenizer_T5, PreTrainedTokenizerFast)
+    set_tokenizer(tokenizer_T5)
+
+
+def test_set_tokenizer_Tokenizer(
+    set_test_config: Callable,
+    cleanup_after_use: Callable,
+    test_session_vars: TestSessionVariables,
+) -> None:
+    """Test that we can use a Tokenizer from tokenizers"""
+    set_test_config(task_type="seq2seq")
+
+    # Check that we can set a generic tokenizer (of type Tokenizer)
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    assert isinstance(tokenizer, Tokenizer)
+    set_tokenizer(tokenizer)
+
+
+def test_set_tokenizer_other(
+    set_test_config: Callable,
+    cleanup_after_use: Callable,
+    test_session_vars: TestSessionVariables,
+) -> None:
+    """
+    Test that we can't use a tokenizer that is not a PreTrainedFastTokenizer from
+    Transformers or a Tokenizer from tokenizers.
+    """
+    set_test_config(task_type="seq2seq")
+
+    # Check that the T5 tokenizer of another type is not working for now
+    tokenizer = tokenizer_T5_not_auto
+    assert not (
+        isinstance(tokenizer, PreTrainedTokenizerFast)
+        or isinstance(tokenizer, Tokenizer)
+    )
+    with raises(ValueError) as context:
+        set_tokenizer(tokenizer_T5_not_auto)
+        assert str(context.value) == (
+            "The tokenizer must be an instance of PreTrainedTokenizerFast "
+            "or Tokenizer"
+        )
