2fb252275e845178c52396e56e52ffff002353e5
Elliott <echartock3@gmail.com>
2024-03-04 14:51:04 -0800

    blacken

--- dataquality/loggers/base_logger.py
+++ dataquality/loggers/base_logger.py
@@ -208,8 +208,7 @@ class BaseGalileoLogger:
         )
 
     @abstractmethod
-    def log(self) -> None:
-        ...
+    def log(self) -> None: ...
 
     @staticmethod
     def _convert_tensor_ndarray(
@@ -327,8 +326,7 @@ class BaseGalileoLogger:
         pm = PatchManager()
         pm.unpatch()
 
-    def upload(self) -> None:
-        ...
+    def upload(self) -> None: ...
 
     @classmethod
     def get_all_subclasses(cls: Type[T]) -> List[Type[T]]:
