0dcf13dcf4e8f1e9f2e44bc33323c3b14e8e03bf
Elliott <echartock3@gmail.com>
2024-04-17 11:28:58 -0400

    fix(release): 2.1.1

--- dataquality/__init__.py
+++ dataquality/__init__.py
@@ -30,7 +30,7 @@ If you want to train without a model, you can use the auto framework:
         dataquality.get_insights()
 """
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 import sys
 from typing import Any, List, Optional
