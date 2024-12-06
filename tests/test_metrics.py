d83b1646243058a64ed0a7d5626e75743414ae38
Valeriy Pogrebitskiy <vpogrebi@iname.com>
2024-12-06 18:51:48 -0500

    ... Signed-off-by: Valeriy Pogrebitskiy <vpogrebi@iname.com>

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -197,4 +197,3 @@ def test_get_edited_dataframe_reviewed_only_edits(mocker):
 
     call_df = _process_exported_dataframe_mock.call_args_list[0][0][0]
     assert call_df.reviewers.tolist() == [["review1"]] * 3
-    assert call_df.equals(expected_df)
